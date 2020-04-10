f3dd48ca95cb51c2aa55236b0931fef97c61bcde
Alex Cabrera <alex.cabrera@gmail.com>
2020-04-10 11:08:25 -0400

    line graph

--- src/Graph.svelte
+++ src/Graph.svelte
@@ -1,6 +1,97 @@
 <script>
+  import { onMount } from "svelte";
   import { selected } from "./stores.js";
+  import * as d3 from "d3";
+
+  let el;
+  let w;
+  $: w, drawGraph();
+  onMount(_ => drawGraph());
+  function drawGraph() {
+    // set the dimensions and margins of the graph
+    var margin = { top: 10, right: 30, bottom: 30, left: 60 },
+      width = w - margin.left - margin.right,
+      height = w - margin.top - margin.bottom;
+
+    // append the svg object to the body of the page
+    d3.select(el).html("");
+    var svg = d3
+      .select(el)
+      .append("svg")
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom)
+      .append("g")
+      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+    //Read the data
+    d3.csv("./timeseries.csv", d3.autoValue).then(data => {
+      // Add X axis --> it is a date format
+      var parseTime = d3.timeParse("%Y-%m-%d");
+
+      console.log(data);
+      data.forEach(d => (d.date = parseTime(d.date)));
+      data.sort((a, b) => a.date - b.date);
+      var x = d3
+        .scaleTime()
+        .domain(d3.extent(data, d => d.date))
+        .range([0, width]);
+      svg
+        .append("g")
+        .attr("transform", "translate(0," + height + ")")
+        .call(d3.axisBottom(x));
+
+      // Add Y axis
+      var y = d3
+        .scaleLinear()
+        .domain([0, d3.max(data, d => d.value)])
+        .range([height, 0]);
+      svg.append("g").call(d3.axisLeft(y));
+
+      let line = d3
+        .line()
+        .x(d => x(d.date))
+        .y(d => y(d.value));
+
+      svg
+        .append("path")
+        .data([data])
+        .attr("class", "line")
+        .attr("d", line);
+      // // Show confidence interval
+      // svg
+      //   .append("path")
+      //   .datum(data)
+      //   .attr("fill", "#cce5df")
+      //   .attr("stroke", "none")
+      //   .attr(
+      //     "d",
+      //     d3
+      //       .area()
+      //       .x(function(d) {
+      //         return x(d.x);
+      //       })
+      //       .y0(function(d) {
+      //         return y(d.CI_right);
+      //       })
+      //       .y1(function(d) {
+      //         return y(d.CI_left);
+      //       })
+      //   );
+
+      // Add the line
+      // svg
+      //   .append("path")
+      //   .datum(data)
+      //   .attr("fill", "none")
+      //   .attr("stroke", "steelblue")
+      //   .attr("stroke-width", 1.5)
+      //   .attr("d");
+    });
+  }
 </script>
 
 <p>cool line graph with options</p>
 <p>currently hovering on {$selected}</p>
+<div bind:clientWidth={w}>
+  <div id="map" bind:this={el} />
+</div>
