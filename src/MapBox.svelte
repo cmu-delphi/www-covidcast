e265da38ba34e36e23ed1d8f8758ba35041b9c57
Alex Cabrera <alex.cabrera@gmail.com>
2020-04-11 12:11:56 -0400

    counties on mapbox

--- src/MapBox.svelte
+++ src/MapBox.svelte
@@ -1,5 +1,6 @@
 <script>
   import { onMount, setContext } from "svelte";
+  import { selected } from "./stores.js";
   import mapboxgl from "mapbox-gl";
 
   // https://docs.mapbox.com/help/glossary/access-token/
@@ -27,53 +28,53 @@
       });
 
       map.on("load", function() {
-        map.addSource("population", {
-          type: "vector",
-          url: "mapbox://mapbox.660ui7x6"
+        map.addSource("counties", {
+          type: "geojson",
+          data: "./gz_2010_us_050_00_5m.json"
         });
-        console.log(map);
-        map.addLayer(
-          {
-            id: "state-population",
-            source: "population",
-            "source-layer": "state_county_population_2014_cen",
-            type: "fill",
-            filter: ["==", "isState", true],
-            paint: {
-              "fill-color": [
-                "interpolate",
-                ["linear"],
-                ["get", "population"],
-                0,
-                "#F2F12D",
-                500000,
-                "#EED322",
-                750000,
-                "#E6B71E",
-                1000000,
-                "#DA9C20",
-                2500000,
-                "#CA8323",
-                5000000,
-                "#B86B25",
-                7500000,
-                "#A25626",
-                10000000,
-                "#8B4225",
-                25000000,
-                "#723122"
-              ],
-              "fill-opacity": 0.75
-            }
-          },
-          "waterway-label"
-        );
-        map.on("click", "state-population", function(e) {
-          new mapboxgl.Popup()
-            .setLngLat(e.lngLat)
-            .setHTML(e.features[0].properties.name)
-            .addTo(map);
+        console.log(map.getSource("counties"));
+        map.addLayer({
+          id: "counties-level",
+          source: "counties",
+          // "source-layer": "state_county_population_2014_cen",
+          type: "line"
+          // filter: ["==", "isState", true],
+          //   paint: {
+          //     "fill-color": [
+          //       "interpolate",
+          //       ["linear"],
+          //       ["get", "population"],
+          //       0,
+          //       "#F2F12D",
+          //       500000,
+          //       "#EED322",
+          //       750000,
+          //       "#E6B71E",
+          //       1000000,
+          //       "#DA9C20",
+          //       2500000,
+          //       "#CA8323",
+          //       5000000,
+          //       "#B86B25",
+          //       7500000,
+          //       "#A25626",
+          //       10000000,
+          //       "#8B4225",
+          //       25000000,
+          //       "#723122"
+          //     ],
+          //     "fill-opacity": 0.75
+          //   }
+          // },
+          // "waterway-label"
         });
+      });
+      map.on("click", "counties-level", function(e) {
+        selected.set(e.features[0].properties.NAME);
+        new mapboxgl.Popup()
+          .setLngLat(e.lngLat)
+          .setHTML(e.features[0].properties.NAME)
+          .addTo(map);
       });
     };
 
