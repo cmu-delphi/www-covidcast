6b2960a5547dcb924fab39e153a704d22dc4ed3d
Andrew Kuznetsov <akuz.dev@gmail.com>
2020-04-13 15:51:37 -0400

    Added initial work on Albers-USA and reprojection of HRR

--- src/MapBox.svelte
+++ src/MapBox.svelte
@@ -30,7 +30,7 @@
     map = new mapboxgl.Map({
       attributionControl: false,
       container,
-      style: "./map_styles/mapbox_streets_v11_original.json",
+      style: "./map_styles/mapbox_albers_usa_style.json",
       center: [LON, LAT],
       zoom: ZOOM,
       // maxBounds: new mapboxgl.LngLatBounds([-171.791110603, 18.91619], [-66.96466, 71.3577635769]),  // geo coords bounds of US (including Alaska, Hawaii)
