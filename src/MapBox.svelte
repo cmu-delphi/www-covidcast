d9f89931bda987514a550c1249f5c2c7c720dace
Alex Cabrera <alex.cabrera@gmail.com>
2020-04-18 17:55:26 -0400

    small change for scale

--- src/MapBox.svelte
+++ src/MapBox.svelte
@@ -137,7 +137,7 @@
         data: $geojsons.get('city'),
         cluster: true,
         clusterMaxZoom: 14, // Max zoom to cluster points on
-        clusterRadius: 50, // Radius of each cluster when clustering points (defaults to 50),
+        clusterRadius: 1000, // Radius of each cluster when clustering points (defaults to 50),
         clusterProperties: {
           largest: [
             [
