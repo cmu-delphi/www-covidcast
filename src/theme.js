ef780a997780015141f8a3f24718f49a44d40c8b
Alex Cabrera <alex.cabrera@gmail.com>
2020-04-19 18:48:09 -0400

    fix tooltip on line chart

--- src/theme.js
+++ src/theme.js
@@ -1,14 +1,14 @@
 export const DIRECTION_THEME = {
   decreasing: 'rgb(72, 167, 236)',
   steady: '#f0f0f0',
-  increasing: '#ffae90',
+  increasing: '#fda96e',
   max: '#ffae90',
-  gradientMin: 'rgb(255, 237, 160)',
-  gradientMiddle: 'rgb(252, 78, 42)',
-  gradientMax: 'rgb(128, 0, 38)',
   // gradientMin: 'rgba(170, 102, 0, .3)',
   // gradientMiddle: 'rgba(204, 0, 0, .65)',
   // gradientMax: 'rgb(204, 0, 0)',
+  gradientMin: 'rgb(255, 237, 160)',
+  gradientMiddle: 'rgb(252, 78, 42)',
+  gradientMax: 'rgb(128, 0, 38)',
 };
 
 // export const DIRECTION_THEME = {
