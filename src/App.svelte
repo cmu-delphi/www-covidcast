6a4c7a0785c0e04f589d997590afdfecdf2319b7
lxieyang <lxieyang@umich.edu>
2020-04-19 16:36:08 -0400

    no more fake data and network requests

--- src/App.svelte
+++ src/App.svelte
@@ -32,53 +32,6 @@
   let error = null;
   let changingSensor = false;
 
-  // this is for graph dev purposes
-  let use_real_data = true;
-
-  // if (use_real_data === false) {
-  //   console.log('using fake network requests');
-  //   onMount(_ => {
-  //     d3.json('./temp_graph_data/meta_request_results.json').then(meta => {
-  //       metaStats.set(meta.epidata);
-
-  //       let queries = [];
-  //       let entries = [];
-  //       let timeMap = new Map();
-  //       $sensors.forEach(sens => {
-  //         let date = meta.epidata.find(d => d.data_source === sens.id);
-  //         let minDate = date.min_time;
-  //         let maxDate = date.max_time;
-  //         timeMap.set(sens.id, [minDate, maxDate]);
-  //         sens.levels.forEach(l => {
-  //           entries.push([sens.id, l]);
-  //         });
-  //       });
-
-  //       // faking queries with d3.json()
-  //       queries.push(d3.json('./temp_graph_data/fb_survey_cli_county_20200406-20200413.json'));
-  //       queries.push(d3.json('./temp_graph_data/fb_survey_cli_msa_20200406-20200413.json'));
-  //       queries.push(d3.json('./temp_graph_data/google-survey_cli_county_20200411-20200416.json'));
-  //       queries.push(d3.json('./temp_graph_data/quidel_negativeratio_county_20200201-20200409.json'));
-  //       queries.push(d3.json('./temp_graph_data/quidel_negativeratio_msa_20200201-20200409.json'));
-  //       queries.push(d3.json('./temp_graph_data/ght_smoothedsearch_msa_20200201-20200412.json'));
-
-  //       let dat = {};
-  //       Promise.all(queries).then(d => {
-  //         console.log(d);
-  //         entries.forEach((ent, i) => {
-  //           dat[ent[0]] ? '' : (dat[ent[0]] = {});
-  //           dat[ent[0]][ent[1]] = d[i].epidata;
-  //         });
-  //         times.set(timeMap);
-  //         data.set(dat);
-  //       });
-  //     });
-  //   }).catch(err => {
-  //     error = err;
-  //     currentDataReadyOnMay.set(true);
-  //   });
-  // }
-
   function updateRegionSliceCache(sensor, level, date, reason = 'unspecified') {
     console.log($regionSliceCache);
     if (!$mounted) return;
@@ -197,61 +150,57 @@
     updateTimeSliceCache($currentSensor, $currentLevel, r);
   });
 
-  if (use_real_data) {
-    onMount(_ => {
-      fetch(ENDPOINT_META)
-        .then(d => d.json())
-        .then(meta => {
-          metaData.set(meta.epidata);
-          let timeMap = new Map();
-          $sensors.forEach(s => {
-            let matchedMeta = meta.epidata.find(
-              d => d.data_source === s.id && d.signal === s.signal && d.time_type === 'day',
-            );
-            timeMap.set(s.id, [matchedMeta.min_time, matchedMeta.max_time]);
-          });
-          times.set(timeMap);
-
-          let l = $currentLevel;
-          if (!$sensors.find(d => d.id === $currentSensor).levels.includes($currentLevel)) {
-            console.log('update?');
-            l = $sensors.find(d => d.id === $currentSensor).levels[0];
-            currentLevel.set(l);
-          }
-
-          let q =
-            ENDPOINT +
-            '&data_source=' +
-            $currentSensor +
-            '&signal=' +
-            $sensors.find(d => d.id === $currentSensor).signal +
-            '&geo_type=' +
-            l +
-            '&time_values=' +
-            timeMap.get($currentSensor)[1] +
-            '&geo_value=*';
-          fetch(q)
-            .then(d => d.json())
-            .then(d => {
-              console.log(q, d);
-              if (d.result < 0 || d.message.includes('no results')) {
-                console.log('bad api call, not updating regionSliceCache');
-                currentData.set([]);
-                regionSliceCache.update(m =>
-                  m.set($currentSensor + $currentLevel + timeMap.get($currentSensor)[1], []),
-                );
-              } else {
-                currentData.set(d.epidata);
-                regionSliceCache.update(m =>
-                  m.set($currentSensor + $currentLevel + timeMap.get($currentSensor)[1], d.epidata),
-                );
-              }
-
-              mounted.set(1);
-            });
+  onMount(_ => {
+    fetch(ENDPOINT_META)
+      .then(d => d.json())
+      .then(meta => {
+        metaData.set(meta.epidata);
+        let timeMap = new Map();
+        $sensors.forEach(s => {
+          let matchedMeta = meta.epidata.find(
+            d => d.data_source === s.id && d.signal === s.signal && d.time_type === 'day',
+          );
+          timeMap.set(s.id, [matchedMeta.min_time, matchedMeta.max_time]);
         });
-    });
-  }
+        times.set(timeMap);
+
+        let l = $currentLevel;
+        if (!$sensors.find(d => d.id === $currentSensor).levels.includes($currentLevel)) {
+          console.log('update?');
+          l = $sensors.find(d => d.id === $currentSensor).levels[0];
+          currentLevel.set(l);
+        }
+
+        let q =
+          ENDPOINT +
+          '&data_source=' +
+          $currentSensor +
+          '&signal=' +
+          $sensors.find(d => d.id === $currentSensor).signal +
+          '&geo_type=' +
+          l +
+          '&time_values=' +
+          timeMap.get($currentSensor)[1] +
+          '&geo_value=*';
+        fetch(q)
+          .then(d => d.json())
+          .then(d => {
+            console.log(q, d);
+            if (d.result < 0 || d.message.includes('no results')) {
+              console.log('bad api call, not updating regionSliceCache');
+              currentData.set([]);
+              regionSliceCache.update(m => m.set($currentSensor + $currentLevel + timeMap.get($currentSensor)[1], []));
+            } else {
+              currentData.set(d.epidata);
+              regionSliceCache.update(m =>
+                m.set($currentSensor + $currentLevel + timeMap.get($currentSensor)[1], d.epidata),
+              );
+            }
+
+            mounted.set(1);
+          });
+      });
+  });
 </script>
 
 <style>
