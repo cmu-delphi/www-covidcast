d9db68a2e8e32419c707b564a3f41e87cb4814be
krismawil <7612737+krismawil@users.noreply.github.com>
2020-04-19 13:00:10 -0400

    tweaks to graph from Sunday morning feedback

--- src/theme.js
+++ src/theme.js
@@ -5,8 +5,8 @@
 // };
 
 export const DIRECTION_THEME = {
-  decreasing: '#7bcce7',
-  steady: '#f0f0f0',
-  increasing: '#ffae90',
-  max: '#ffae90',
+  decreasing: '#006677',
+  steady: '#E0E0E0',
+  increasing: '#AA6600',
+  max: '#AA6600',
 };
