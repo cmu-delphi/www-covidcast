39a6737d56cd99d3d9caa5fd06174c46067e2184
Adam Perer <adamperer@cmu.edu>
2020-06-19 15:56:23 -0400

    changed color palette for increasing/decreasing

--- src/theme.js
+++ src/theme.js
@@ -1,33 +1,33 @@
-const color_low = 'rgb(255, 237, 160)';
-const color_medium = 'rgb(253, 96, 65)';
-const color_high = 'rgb(128, 0, 0)';
-const color_low_mega = 'rgba(255, 237, 160, 0.5)';
-const color_medium_mega = 'rgba(253, 96, 65, 0.5)';
-const color_high_mega = 'rgba(128, 0, 0, 0.5)';
-const color_graph_line = '#767676';
+const color_decreasing = 'rgb(86,157,201)'; //''rgb(0,223,252)'//'#badee8';
+const color_steady = 'rgb(220, 220, 220)'; // 'rgb(249,212,35)' //'#f2df91';
+const color_increasing = 'rgb(218,107,87)'; //rgb(255,78,80)' //'#ce0a05';
+
+const color_decreasing_mega = 'rgba(86,157,201,0.5)'; //'#badee8';
+const color_steady_mega = 'rgba(220, 220, 220,0.5)'; //'#f2df91';
+const color_increasing_mega = 'rgba(218,107,87, 0.5)'; //'#ce0a05';
 
 export const DIRECTION_THEME = {
-  increasing: color_high,
-  steady: color_medium,
-  decreasing: color_low,
+  increasing: color_increasing, //color_high,
+  steady: color_steady, //color_medium,
+  decreasing: color_decreasing, //color_low,
   increasingIcon: '&#9650;',
   steadyIcon: '&#9644;',
   decreasingIcon: '&#9660;',
-  max: color_high,
+  max: color_increasing, //color_high,
   countMin: 'rgb(242,242,242)',
-  gradientMin: color_low,
-  gradientMiddle: color_medium,
-  gradientMax: color_high,
-  gradientMinMega: color_low_mega,
-  gradientMiddleMega: color_medium_mega,
-  gradientMaxMega: color_high_mega,
+  gradientMin: color_decreasing,
+  gradientMiddle: color_steady,
+  gradientMax: color_increasing,
+  gradientMinMega: color_decreasing_mega,
+  gradientMiddleMega: color_steady_mega,
+  gradientMaxMega: color_increasing_mega,
 };
 
 export const MAP_THEME = {
   selectedRegionOutline: '#333333', //'#000000',
   hoverRegionOutline: '#cccccc', //'#ffffff',
   stateOutline: '#ffffff', //'#bcbcbc',
-  countyFill: '#e4dac4',
+  countyFill: '#eeeeee', //'#e4dac4',
   countyOutline: '#ffffff', //'#e0e0e0',
   countyOutlineWhenFilled: '#ffffff' //'#616161',
 };
