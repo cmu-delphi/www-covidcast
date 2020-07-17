924cf5a4b73ebccd9e9185af59d4644998cf96ac
Samuel Gratzl <sam@sgratzl.com>
2020-07-17 11:53:12 +0200

    enable workbox for service worker

--- src/index.js
+++ src/index.js
@@ -10,3 +10,16 @@ const app = new App({
 });
 
 export default app;
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker
+      .register('/service-worker.js')
+      .then((registration) => {
+        console.log('SW registered: ', registration);
+      })
+      .catch((registrationError) => {
+        console.log('SW registration failed: ', registrationError);
+      });
+  });
+}
--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,7 @@ const CopyPlugin = require('copy-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const HtmlWebpackHarddiskPlugin = require('html-webpack-harddisk-plugin');
+const WorkboxPlugin = require('workbox-webpack-plugin');
 
 const devMode = process.env.NODE_ENV !== 'production';
 
@@ -93,6 +94,13 @@ module.exports = () => {
         chunkFilename: 'build/[id].css',
       }),
       !devMode && new CleanWebpackPlugin(),
+      !devMode &&
+        new WorkboxPlugin.GenerateSW({
+          // these options encourage the ServiceWorkers to get in there fast
+          // and not allow any straggling "old" SWs to hang around
+          clientsClaim: true,
+          skipWaiting: true,
+        }),
     ].filter(Boolean),
   };
 };
