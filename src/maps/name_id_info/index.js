337ce866a2e7f8a92492e40aa28dff4297f0b0fc
Samuel Gratzl <sam@sgratzl.com>
2020-07-23 13:13:13 +0200

    store and restore a csv format
    # Conflicts:
    #	webpack.config.js

--- src/maps/name_id_info/index.js
+++ src/maps/name_id_info/index.js
@@ -1,25 +1,25 @@
+import { dsvFormat } from 'd3';
+
 export default function loadNameIdInfo() {
-  return import('./name_id_info_compressed.json').then((mod) => {
+  return import('./name_id_info.csv').then((mod) => {
+    /**
+     * @type string
+     */
     const raw = mod.default;
     /**
-     * @type {{display_name: string, id: string, property_id: string, level: string}[]}
+     * @type {{name: string; display_name: string, id: string, property_id: string, level: string}[]}
      */
-    const data = [];
+    const data = dsvFormat(';').parse(raw);
 
     // decompress the values again
-    Object.keys(raw).forEach((level) => {
-      const values = raw[level];
-      for (const value of values) {
-        // recreate old structure
-        value.level = level;
-        value.display_name = value.dname || value.id;
-        delete value.dname;
-        if (value.property_id == null) {
-          value.property_id = value.id;
-        }
-        data.push(value);
+    for (const row of data) {
+      if (!row.display_name) {
+        row.display_name = row.name;
       }
-    });
+      if (!row.property_id) {
+        row.property_id = row.id;
+      }
+    }
     return data;
   });
 }
