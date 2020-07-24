76a93dba308e451588d53bd2325d8e8afdb79974
Samuel Gratzl <sam@sgratzl.com>
2020-07-24 19:41:51 +0200

    simple header menu

--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import 'regenerator-runtime/runtime';
 import './global.css';
 import App from './components/App.svelte';
 
+document.querySelector('.viz-menu-button').addEventListener('click', (e) => {
+  e.preventDefault();
+  e.currentTarget.classList.toggle('open');
+});
+
 const hostElement = document.querySelector('#vizbox') || document.body;
 
 const app = new App({
