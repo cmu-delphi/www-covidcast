532ca23912bc5de95de882fe897bc6609e34b225
Samuel Gratzl <sam@sgratzl.com>
2020-07-31 12:18:10 +0200

    feat: fix vega embedding
    extract line chart spec for better completion
    fix padding

--- src/components/vega/LineChart.svelte
+++ src/components/vega/LineChart.svelte
@@ -1,5 +1,6 @@
 <script>
   import Vega from './Vega.svelte';
+  import lineSchema from './LineChart.json';
   /**
    * @typedef {import('../stores/fetchData').EpiDataRow} EpiDataRow
    */
@@ -9,75 +10,7 @@
    */
   export let data = [];
 
-  export const schema = {
-    $schema: 'https://vega.github.io/schema/vega-lite/v4.json',
-    data: {
-      values: null,
-    },
-    // mark: 'line',
-    encoding: {
-      color: {
-        value: 'grey',
-      },
-      x: {
-        field: 'time_value',
-        type: 'temporal',
-        axis: null,
-      },
-      y: {
-        field: 'value',
-        type: 'quantitative',
-        axis: null,
-      },
-    },
-    layer: [
-      {
-        mark: {
-          type: 'line',
-          interpolate: 'monotone',
-        },
-      },
-      {
-        selection: {
-          index: {
-            type: 'single',
-            on: 'mousemove',
-            encodings: ['x'],
-            nearest: true,
-          },
-        },
-        mark: { type: 'point' },
-        encoding: {
-          y: { field: 'value', type: 'quantitative' },
-          opacity: { value: 0 },
-        },
-      },
-      {
-        transform: [
-          {
-            filter: {
-              and: ['index.date_value', { selection: 'index' }],
-            },
-          },
-        ],
-        mark: 'rule',
-      },
-      {
-        transform: [
-          {
-            filter: {
-              and: ['index.date_value', { selection: 'index' }],
-            },
-          },
-        ],
-        mark: 'text',
-        encoding: {
-          y: { value: 10 },
-          text: { field: 'date_value', type: 'temporal' },
-        },
-      },
-    ],
-  };
+  const schema = lineSchema;
 </script>
 
 <Vega {data} on: {schema} />
--- src/components/vega/Vega.svelte
+++ src/components/vega/Vega.svelte
@@ -6,9 +6,9 @@
    */
 
   /**
-   * @type {EpiDataRow[]}
+   * @type {Promise<EpiDataRow[]>}
    */
-  export let data = [];
+  export let data = Promise.resolve([]);
 
   /**
    * @type {import('vega-embed').VisualizationSpec}
@@ -23,38 +23,61 @@
    * @type {import('vega-embed').Result | null}
    */
   let vega = null;
+  /**
+   * @type {Promise<import('vega-embed').Result> | null}
+   */
+  let vegaPromise = null;
+
+  let loading = true;
 
   $: updateData();
 
   function updateData() {
-    if (!vega) {
+    if (!vegaPromise) {
       return;
     }
-    vega.view.change(
-      'values',
+    loading = true;
+    Promise.all([vegaPromise, data]).then(([vega, d]) => {
       vega.view
-        .changeset()
-        .remove(() => true)
-        .insert(data),
-    );
+        .change(
+          'values',
+          vega.view
+            .changeset()
+            .remove(() => true)
+            .insert(d || []),
+        )
+        .runAsync();
+      loading = false;
+    });
   }
 
   onMount(() => {
     const bb = root.getBoundingClientRect();
-    embed(root, schema, { actions: false, width: bb.width, height: bb.height })
-      .then((r) => {
-        vega = r;
-        updateData();
-      })
-      .catch((error) => console.error(error));
+    // don't know why the size diff
+    const margin = 6;
+    vegaPromise = embed(root, schema, {
+      actions: false,
+      width: bb.width - margin,
+      height: bb.height - margin,
+      padding: 0,
+    });
+    vegaPromise.then((r) => {
+      vega = r;
+      updateData();
+    });
+    vegaPromise.catch((error) => console.error(error));
   });
 
   onDestroy(() => {
     if (vega) {
       vega.finalize();
       vega = null;
+      vegaPromise = null;
+    } else if (vegaPromise) {
+      vegaPromise.then((r) => r.finalize());
+      vegaPromise = null;
     }
   });
 </script>
 
-<div bind:this={root} />
+<div bind:this={root} class:loading />
