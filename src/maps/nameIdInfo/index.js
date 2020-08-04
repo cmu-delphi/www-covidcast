26fc3a32eb2064fccf4e705aa53dc262387ad2b4
Samuel Gratzl <sam@sgratzl.com>
2020-08-04 14:53:08 +0200

    better types and add currentRegionInfo

--- src/maps/nameIdInfo/index.js
+++ src/maps/nameIdInfo/index.js
@@ -3,9 +3,9 @@ import { dsvFormat } from 'd3';
 /**
  * @typedef {object} NameInfo
  * @property {name_id_info} direction
- * @property {string} name name to search
- * @property {string} display_name name to show
- * @property {string} id ?
+ * @property {string} display_name name to show and search
+ * @property {string} name name for param
+ * @property {string} id param id
  * @property {string} property_id geojson: feature.property.id
  * @property {'state' | 'county' | 'msa'} level
  */
--- src/stores/index.js
+++ src/stores/index.js
@@ -3,6 +3,7 @@ import { injectIDs } from '../util';
 import { LogScale, SqrtScale } from './scales';
 import * as d3 from 'd3';
 import { sensorList, withSensorEntryKey } from './constants';
+import { regionSearchList } from './search';
 export {
   dict,
   specialCounties,
@@ -153,6 +154,18 @@ export const currentRegion = writable('', (set) => {
   }
 });
 export const currentRegionName = writable('');
+
+/**
+ * current region info (could also be null)
+ */
+export const currentRegionInfo = derived([currentRegion, regionSearchList], ([current, search]) => {
+  console.log(
+    'derive current',
+    current,
+    search.find((d) => d.id == current),
+  );
+  return search.find((d) => d.id == current) || null;
+});
 
 // currently only supporting 'swpa' - South western Pennsylvania
 export const currentZone = writable('', (set) => {
