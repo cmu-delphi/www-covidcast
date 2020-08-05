bc615966734b5c9ed3fe0fc57d8c6cab2c3e18fc
Samuel Gratzl <sam@sgratzl.com>
2020-08-05 11:38:03 +0200

    split legends into individual files
    # Conflicts:
    #	src/components/Legend.svelte

---
+++ src/components/legends/BubbleLegend.svelte
@@ -0,0 +1,66 @@
+<script>
+  import { ENCODING_BUBBLE_THEME } from '../../theme';
+  import { stats, currentLevel, colorScale, bubbleRadiusScale, currentSensorEntry } from '../../stores';
+  import { transparent } from '../../util';
+  import { getSigfigs, generateLabels } from './utils';
+
+  let high = '';
+  let unit = '';
+  let labels = [['0', '0']];
+
+  $: {
+    const r = generateLabels($stats, $currentSensorEntry, $currentLevel);
+    labels = r.labels;
+    high = r.high;
+    unit = r.unit;
+  }
+</script>
+
+<style>
+  .bubble-legend {
+    margin: 0;
+    padding: 0;
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+  }
+
+  .bubble-legend li {
+    display: flex;
+    font-size: 80%;
+    padding-right: 10px;
+    align-items: center;
+  }
+
+  .bubble {
+    border: 1px solid #666;
+    border-radius: 50%;
+    display: inline-block;
+    margin-right: 0.3rem;
+    max-width: 200px;
+    max-height: 200px;
+  }
+</style>
+
+<ul class="bubble-legend">
+  {#each labels as [label], j}
+    {#if +label > 0}
+      <li class="colored">
+        <div
+          style="width: {$bubbleRadiusScale(+label) * 2}px; height: {$bubbleRadiusScale(+label) * 2}px;background: {transparent($colorScale(+label), ENCODING_BUBBLE_THEME.opacity)};border-color:
+          {$colorScale(+label)}"
+          class="bubble" />
+        <div>{getSigfigs(label, 3)}</div>
+      </li>
+    {/if}
+  {/each}
+  {#if high}
+    <li class="colored">
+      <div
+        style="background: {transparent($colorScale(+high), ENCODING_BUBBLE_THEME.opacity)};width: {$bubbleRadiusScale(+high) * 2}px;
+        height: {$bubbleRadiusScale(+high) * 2}px;border-color: {$colorScale(+high)}"
+        class="bubble" />
+      <div>{high ? high + unit + '+' : ''}</div>
+    </li>
+  {/if}
+</ul>
