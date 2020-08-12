1d6ff381dc249c34c2f789a4f712959c503f8b81
Samuel Gratzl <sam@sgratzl.com>
2020-08-12 11:35:32 +0200

    feat: finish processing data

--- src/maps/index.js
+++ src/maps/index.js
@@ -2,77 +2,114 @@ import { dsvFormat } from 'd3';
 import { feature } from 'topojson-client';
 import stateRaw from './processed/state.csv';
 import msaRaw from './processed/msa.csv';
+import countyRaw from './processed/county.csv';
+import citiesRaw from './processed/cities.csv';
 
 /**
  * @typedef {object} NameInfo
- * @property {string} display_name name to show and search
  * @property {string} name name for param
+ * @property {string} displayName name to show and search
  * @property {string} id param id
- * @property {string} property_id geojson: feature.property.id
+ * @property {string} propertyId geojson: feature.property.id
  * @property {number} population
- * @property {number} area
+ * @property {number} lat center latitude
+ * @property {number} long center longitude
  * @property {'state' | 'county' | 'msa'} level
  */
 
-function parseCSV(csv) {
+function parseCSV(csv, level) {
   /**
-   * @type {{id: string,name:string,population:number,lat:number,long:number}[]}
+   * @type {NameInfo[]}
    */
   const r = dsvFormat(',').parse(csv, (r) => {
     return Object.assign(r, {
-      population: Number.parseInt(r.population, 10),
+      level,
+      displayName: r.displayName || r.name,
+      propertyId: r.postal || r.id,
+      population: r.population === 'NaN' || r.population === '' ? null : Number.parseInt(r.population, 10),
       lat: Number.parseFloat(r.lat),
       long: Number.parseFloat(r.long),
     });
   });
   return r;
 }
-/**
- * @type {{id: string,postal:string,name:string,population:number,lat:number,long:number}[]}
- */
-const stateInfo = parseCSV(stateRaw);
-const msaInfo = parseCSV(msaRaw);
+const stateInfo = parseCSV(stateRaw, 'state');
+const msaInfo = parseCSV(msaRaw, 'msa');
+const countyInfo = parseCSV(countyRaw, 'county');
 
-function generateGeo(topo, level, arr, propertyIdGen = (d) => d.id) {
-  const centers = {
-    type: 'FeatureCollection',
-    features: arr.map((d) => ({
+export const nameInfos = stateInfo
+  .concat(msaInfo, countyInfo)
+  .sort((a, b) => a.displayName.localeCompare(b.displayName));
+
+function generateGeo(topo, level, arr, additionalProperties = {}) {
+  const byId = new Map(arr.map((s) => [s.id, s]));
+
+  const centers = [];
+  const border = feature(topo, level);
+
+  for (const f of border.features) {
+    const d = byId(f.id);
+    const properties = {
+      ...d,
+      id: d.propertyId,
+      ...additionalProperties,
+    };
+
+    f.properties = properties;
+
+    centers.push({
       type: 'Feature',
       geometry: {
         type: 'Point',
         coordinates: [d.lat, d.long],
       },
       id: d.id,
-      properties: {
-        level,
-        id: propertyIdGen(d),
-        name: d.name,
-        population: d.population,
-      },
-    })),
-  };
-
-  const border = feature(topo, level);
-  const byId = new Map(arr.map((s) => [s.id, s]));
-
-  for (const f of border.features) {
-    const d = byId(f.id);
-    Object.assign(f, {
-      properties: {
-        level,
-        id: propertyIdGen(d),
-        name: d.name,
-        population: d.population,
-      },
+      properties,
     });
   }
-  return { centers, border };
+
+  return {
+    centers: {
+      type: 'FeatureCollection',
+      features: centers,
+    },
+    border,
+  };
 }
 
-export function stateGeo() {
-  return import('./processed/state.geojson.json').then((r) => generateGeo(r, 'state', stateInfo, (d) => d.postal));
+export function stateGeo(additionalProperties = {}) {
+  return import('./processed/state.geojson.json').then((r) => generateGeo(r, 'state', stateInfo, additionalProperties));
 }
 
-export function msaGeo() {
-  return import('./processed/msa.geojson.json').then((r) => generateGeo(r, 'msa', msaInfo));
+export function msaGeo(additionalProperties = {}) {
+  return import('./processed/msa.geojson.json').then((r) => generateGeo(r, 'msa', msaInfo, additionalProperties));
+}
+
+export function countyGeo(additionalProperties = {}) {
+  return import('./processed/county.geojson.json').then((r) =>
+    generateGeo(r, 'county', countyInfo, additionalProperties),
+  );
+}
+
+export function zoneGeo() {
+  return import('./processed/zone.geojson.json').then((r) => ({
+    border: feature(r, 'zone'),
+  }));
+}
+
+export function citiesGeo() {
+  return Promise.resolve({
+    type: 'FeatureCollection',
+    features: dsvFormat(',').parse(citiesRaw, (r) => ({
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [Number.parseFloat(r.lat), Number.parseFloat(r.long)],
+      },
+      properties: {
+        displayName: r.name,
+        population: r.population === 'NaN' || r.population === '' ? null : Number.parseInt(r.population, 10),
+      },
+    })),
+  });
 }
--- src/maps/process.js
+++ src/maps/process.js
@@ -1,4 +1,4 @@
-// Node script to compress the values in the given JSON for smaller bundle size
+// Node script to process the maps and info and generate an optimized version
 
 const { dsvFormat } = require('d3');
 const fs = require('fs');
@@ -7,11 +7,11 @@ const { topology } = require('topojson-server');
 
 const QUANTIZATION = 1e4;
 
-const data = require('./nameIdInfo/name_id_info.json');
+const data = require('./raw/name_id_info.json');
 const rows = data.all;
 
 function states(level = 'state') {
-  const geo = require(`../static/maps/new_states.json`);
+  const geo = require(`./raw/new_states.json`);
 
   const infos = geo.features.map((feature) => {
     const id = feature.properties.STATE;
@@ -27,12 +27,13 @@ function states(level = 'state') {
       long: props.LONG,
     };
   });
-  const topo = topology({ [level]: geo }, QUANTIZATION);
-  fs.writeFileSync(path.resolve(__dirname, `./processed/${level}.geojson.json`), JSON.stringify(topo));
   fs.writeFileSync(
     path.resolve(__dirname, `./processed/${level}.csv`),
     dsvFormat(',').format(infos, ['id', 'postal', 'name', 'population', 'lat', 'long']),
   );
+  const topo = topology({ [level]: geo }, QUANTIZATION);
+  fs.writeFileSync(path.resolve(__dirname, `./processed/${level}.geojson.json`), JSON.stringify(topo));
+  return geo;
 }
 
 function msa(level = 'msa') {
@@ -50,14 +51,14 @@ function msa(level = 'msa') {
   //   updated_at: '2015-03-23T13:36:50Z',
   //   Population: '143476',
   // };
-  const geo = require(`../static/maps/new_${level}.json`);
+  const geo = require(`./raw/new_msa.json`);
 
   // const center = {
   //   type: 'Feature',
   //   geometry: { type: 'Point', coordinates: [-12.145786670984238, -1.8446709502002365] },
   //   properties: { NAME: 'Flagstaff, AZ', GEO_ID: '22380', id: '22380', Population: 143476 },
   // };
-  const centers = require(`../static/maps/${level}_centers.json`).features;
+  const centers = require(`./raw/msa_centers.json`).features;
   const centerById = new Map(centers.map((d) => [d.properties.id, d]));
 
   const infos = geo.features.map((feature) => {
@@ -74,13 +75,102 @@ function msa(level = 'msa') {
       long: center.geometry.coordinates[1],
     };
   });
-  const topo = topology({ states: geo }, QUANTIZATION);
-  fs.writeFileSync(path.resolve(__dirname, `./processed/${level}.geojson.json`), JSON.stringify(topo));
   fs.writeFileSync(
     path.resolve(__dirname, `./processed/${level}.csv`),
     dsvFormat(',').format(infos, ['id', 'name', 'population', 'lat', 'long']),
+  );
+  const topo = topology({ states: geo }, QUANTIZATION);
+  fs.writeFileSync(path.resolve(__dirname, `./processed/${level}.geojson.json`), JSON.stringify(topo));
+  return geo;
+}
+
+function counties(level = 'county') {
+  //{
+  //   "display_name": "Valdez-Cordova County, AK",
+  //   "name": "Valdez-Cordova",
+  //   "id": "02261",
+  //   "property_id": "02261",
+  //   "level": "county"
+  // },
+  const infoById = new Map(rows.filter((d) => d.level === level).map((d) => [d.id, d]));
+  // const properties = {
+  //   GEO_ID: '0500000US02261',
+  //   STATE: '02',
+  //   COUNTY: '261',
+  //   NAME: 'Valdez-Cordova',
+  //   LSAD: 'CA',
+  //   CENSUSAREA: 34239.88,
+  //   Population: '9202',
+  // };
+  const geo = require(`./raw/new_counties.json`);
+
+  // const center = {
+  //   type: 'Feature',
+  //   geometry: { type: 'Point', coordinates: [-15.707827062813598, -10.343262810086836] },
+  //   properties: {
+  //     NAME: 'Valdez-Cordova',
+  //     GEO_ID: '0500000US02261',
+  //     STATE: '02',
+  //     COUNTY: '261',
+  //     id: '02261',
+  //     Population: 9202,
+  //   },
+  // };
+  const centers = require(`./raw/county_centers.json`).features;
+  const centerById = new Map(centers.map((d) => [d.properties.id, d]));
+
+  const infos = geo.features.map((feature) => {
+    const id = feature.properties.GEO_ID.slice(-5);
+    const props = feature.properties;
+    feature.id = id;
+    feature.properties = {};
+    const center = centerById.get(id);
+    const info = infoById.get(id);
+    return {
+      id,
+      name: props.NAME,
+      displayName: info ? info.display_name : `${props.NAME} County, ${props.LSAD}`,
+      state: props.STATE,
+      population: Number.parseInt(props.Population, 10),
+      lat: center.geometry.coordinates[0],
+      long: center.geometry.coordinates[1],
+    };
+  });
+  fs.writeFileSync(
+    path.resolve(__dirname, `./processed/${level}.csv`),
+    dsvFormat(',').format(infos, ['id', 'name', 'displayName', 'state', 'population', 'lat', 'long']),
+  );
+  const topo = topology({ states: geo }, QUANTIZATION);
+  fs.writeFileSync(path.resolve(__dirname, `./processed/${level}.geojson.json`), JSON.stringify(topo));
+  return geo;
+}
+
+function zones() {
+  const geo = require(`./raw/new_zones.json`);
+  const topo = topology({ zone: geo }, QUANTIZATION);
+  fs.writeFileSync(path.resolve(__dirname, `./processed/zone.geojson.json`), JSON.stringify(topo));
+  return geo;
+}
+
+function cities() {
+  const geo = require(`./raw/city_data/cities-reprojected.json`);
+  const infos = geo.features.map((feature) => {
+    const props = feature.properties;
+    return {
+      name: props.city,
+      population: props.population,
+      lat: feature.geometry.coordinates[0],
+      long: feature.geometry.coordinates[1],
+    };
+  });
+  fs.writeFileSync(
+    path.resolve(__dirname, `./processed/cities.csv`),
+    dsvFormat(',').format(infos, ['name', 'population', 'lat', 'long']),
   );
 }
 
 states();
 msa();
+counties();
+zones();
+cities();
