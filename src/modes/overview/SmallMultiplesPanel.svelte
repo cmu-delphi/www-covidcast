89002a6dd5381358316877a67846dc0e370b6a27
Samuel Gratzl <sam@sgratzl.com>
2020-08-13 15:34:07 +0200

    squeeze vertical panel

--- src/modes/overview/SmallMultiplesPanel.svelte
+++ src/modes/overview/SmallMultiplesPanel.svelte
@@ -1,42 +1,21 @@
 <script>
   import SmallMultiples from './SmallMultiples.svelte';
-  import { publicSensors, earlySensors, lateSensors, currentRegionInfo, currentSensor } from '../../stores';
-  import IoMdMap from 'svelte-icons/io/IoMdMap.svelte';
+  import { publicSensors, earlySensors, lateSensors, currentSensor } from '../../stores';
   import IoMdExpand from 'svelte-icons/io/IoMdExpand.svelte';
   import { createEventDispatcher } from 'svelte';
-  import ExpandCollapse from '../../components/ExpandCollapse.svelte';
 
   const dispatch = createEventDispatcher();
 
-  // initially collapsed
-  const collapsed = [
+  const remove = [
     'ght-smoothed_search',
-    'fb-survey-smoothed_hh_cmnty_cli',
     'indicator-combination-confirmed_7dav_incidence_num',
     'indicator-combination-deaths_7dav_incidence_num',
     'safegraph-full_time_work_prop',
-    'safegraph-part_time_work_prop',
   ];
 
   export let detail = null;
 
-  $: smSensors = [
-    {
-      id: 'public',
-      label: 'Public Behavior',
-      sensors: $publicSensors,
-    },
-    {
-      id: 'early',
-      label: 'Early Indicators',
-      sensors: $earlySensors,
-    },
-    {
-      id: 'late',
-      label: 'Late Indicators',
-      sensors: $lateSensors,
-    },
-  ];
+  $: sensors = $publicSensors.concat($earlySensors, $lateSensors).filter((d) => !remove.includes(d.key));
 </script>
 
 <style>
@@ -45,49 +24,53 @@
     padding: 0 0 0 0.25em;
   }
 
-  h4,
   h5 {
+    font-size: 0.88rem;
     flex: 1 1 0;
     padding: 0;
+    cursor: pointer;
+    text-decoration: underline;
+  }
+
+  .header {
+    display: flex;
+    align-items: center;
+    padding-bottom: 0.1em;
+    cursor: pointer;
+  }
+
+  li {
+    margin: 0;
+    padding: 0;
+  }
+
+  li:hover .toolbar {
+    opacity: 1;
   }
 
   .toolbar {
     display: flex;
     font-size: 0.7rem;
+    opacity: 0;
+    transition: opacity 0.25s ease;
   }
 </style>
 
-<h3>{$currentRegionInfo ? $currentRegionInfo.display_name : 'No Region Selected'}</h3>
 <ul class="root">
-  {#each smSensors as sensorGroup}
-    <ExpandCollapse let:collapsed={groupCollapsed}>
-      <h4 slot="header">{sensorGroup.label}</h4>
-      <ul>
-        {#each sensorGroup.sensors as sensor}
-          <ExpandCollapse let:collapsed collapsed={collapsed.includes(sensor.key)}>
-            <h5 slot="header" title={sensor.tooltipText}>{sensor.name}</h5>
-            <div slot="toolbar" class="toolbar">
-              <button
-                class="pg-button"
-                title="Show in Map"
-                on:click|stopPropagation={() => currentSensor.set(sensor.key)}
-                class:active={$currentSensor === sensor.key}
-                disabled={$currentSensor === sensor.key}>
-                <IoMdMap />
-              </button>
-              <button
-                class="pg-button"
-                class:active={detail === sensor}
-                on:click|stopPropagation={() => dispatch('show', detail === sensor ? null : sensor)}>
-                <IoMdExpand />
-              </button>
-            </div>
-            {#if !collapsed && !groupCollapsed}
-              <SmallMultiples {sensor} />
-            {/if}
-          </ExpandCollapse>
-        {/each}
-      </ul>
-    </ExpandCollapse>
+  {#each sensors as sensor}
+    <li>
+      <div class="header">
+        <h5 title={sensor.tooltipText} on:click={() => currentSensor.set(sensor.key)}>{sensor.name}</h5>
+        <div class="toolbar">
+          <button
+            class="pg-button"
+            class:active={detail === sensor}
+            on:click|stopPropagation={() => dispatch('show', detail === sensor ? null : sensor)}>
+            <IoMdExpand />
+          </button>
+        </div>
+      </div>
+      <SmallMultiples {sensor} />
+    </li>
   {/each}
 </ul>
