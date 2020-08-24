63fc143ad65d0617e406a6e82bf393760f636326
Samuel Gratzl <sam@sgratzl.com>
2020-08-24 12:14:04 +0200

    sortable columns

--- src/modes/top10/Top10.svelte
+++ src/modes/top10/Top10.svelte
@@ -15,6 +15,7 @@
   import { getInfoByName, nameInfos } from '../../maps';
   import Top10Sensor from './Top10Sensor.svelte';
   import Search from '../../components/Search.svelte';
+  import { levelMegaCounty } from '../../stores/constants';
 
   /**
    * @typedef {import('../../maps').NameInfo} ValueRow
@@ -38,13 +39,34 @@
   $: primary = $currentSensorEntry;
   let showTopN = 10;
   let sortCriteria = 'primary';
+  let sortDirectionDesc = true;
 
-  function bySortCriteria(a, b, _sortCriteria) {
-    // TODO better ranking
-    if (a.primary.value !== b.primary.value) {
-      return a.primary.value < b.primary.value ? 1 : -1;
+  function applyDirection(comparator, sortDirectionDesc) {
+    return sortDirectionDesc ? (a, b) => -comparator(a, b) : comparator;
+  }
+  function bySortCriteria(sortCriteria) {
+    if (sortCriteria === 'primary') {
+      return (a, b) => {
+        if (a.primary.value !== b.primary.value) {
+          return a.primary.value < b.primary.value ? -1 : 1;
+        }
+        return a.displayName.localeCompare(b.displayName);
+      };
     }
-    return a.displayName.localeCompare(b.displayName);
+    if (sortCriteria === 'population') {
+      return (a, b) => {
+        if (a.population !== b.population) {
+          return a.population < b.population ? -1 : 1;
+        }
+        return a.displayName.localeCompare(b.displayName);
+      };
+    }
+    return (a, b) => {
+      if (a.population !== b.population) {
+        return a.population < b.population ? -1 : 1;
+      }
+      return a.displayName.localeCompare(b.displayName);
+    };
   }
 
   // transform current data
@@ -57,14 +79,15 @@
   $: {
     loading = true;
     fetchRegionSlice(primary, $currentLevel, $currentDateObject).then((data) => {
-      rows = data.map((row) => extentData(row)).filter(Boolean);
+      rows = data.map((row) => extentData(row)).filter((d) => d != null && d.level !== levelMegaCounty.id);
       loading = false;
     });
   }
 
+  $: comparator = applyDirection(bySortCriteria(sortCriteria), sortDirectionDesc);
   $: sortedRows = rows
     .slice()
-    .sort((a, b) => bySortCriteria(a, b, sortCriteria))
+    .sort(comparator)
     .map((d, i) => {
       d.rank = i + 1;
       return d;
@@ -83,6 +106,15 @@
 
   function showMore() {
     showTopN = Math.min(rows.length, showTopN + 10);
+  }
+
+  function sortClick(prop, defaultSortDesc = false) {
+    if (sortCriteria === prop) {
+      sortDirectionDesc = !sortDirectionDesc;
+      return;
+    }
+    sortCriteria = prop;
+    sortDirectionDesc = defaultSortDesc;
   }
 </script>
 
@@ -142,6 +174,7 @@
   .table th {
     background: white;
     text-align: center;
+    cursor: pointer;
   }
 
   .table > table {
@@ -166,6 +199,15 @@
 
   .selected > :global(td) {
     border: 2px solid var(--red);
+  }
+
+  .sorted::after {
+    content: '▲';
+    display: inline-block;
+  }
+
+  .desc .sorted::after {
+    content: '▼';
   }
 
   /** mobile **/
@@ -197,12 +239,19 @@
 
   <div class="table base-font-size">
     <table>
-      <thead>
+      <thead class:desc={sortDirectionDesc}>
         <tr>
           <th rowspan="2">#</th>
-          <th rowspan="2">Name</th>
-          <th rowspan="2">Population</th>
-          <th colspan={primary.isCasesOrDeath ? 3 : 2}>{primary.name}</th>
+          <th rowspan="2" class:sorted={sortCriteria === 'name'} on:click={() => sortClick('name')}>Name</th>
+          <th rowspan="2" class:sorted={sortCriteria === 'population'} on:click={() => sortClick('population', true)}>
+            Population
+          </th>
+          <th
+            colspan={primary.isCasesOrDeath ? 3 : 2}
+            class:sorted={sortCriteria === 'primary'}
+            on:click={() => sortClick('primary', true)}>
+            {primary.name}
+          </th>
         </tr>
         <tr>
           <th>{$currentDateObject.toLocaleDateString()}</th>
