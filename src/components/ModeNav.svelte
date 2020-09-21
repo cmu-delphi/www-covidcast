f8a9b287c87a8ec31998de3b3ee50b8ffda1ee90
EJ Fox <ejfox@ejfox.com>
2020-09-21 13:32:55 -0400

    refactor: reorder modes

--- src/components/ModeNav.svelte
+++ src/components/ModeNav.svelte
@@ -2,21 +2,23 @@
   import SingleModeToggle from './SingleModeToggle.svelte';
   import { modeByID } from '../modes';
 
-  export let mode = modeByID.overview;
+  // let mode = modeByID.overview;
   // export let label = mode.label;
 </script>
 
 <style>
   .mode-container {
+    /* text-align: center; */
     margin-left: 0.5em;
+    /* border-bottom: 1px solid #ccc; */
   }
 </style>
 
 <nav>
   <div class="mode-container">
     <SingleModeToggle mode={modeByID.overview} />
-    <SingleModeToggle mode={modeByID.export} />
-    <SingleModeToggle mode={modeByID.top10} />
     <SingleModeToggle mode={modeByID.timelapse} />
+    <SingleModeToggle mode={modeByID.top10} />
+    <SingleModeToggle mode={modeByID.export} />
   </div>
 </nav>
