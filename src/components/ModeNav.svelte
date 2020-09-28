d030776394fe724b931e84cdee421c3c071a56ee
Samuel Gratzl <sam@sgratzl.com>
2020-09-28 11:45:24 +0200

    style: mobile nav bar

--- src/components/ModeNav.svelte
+++ src/components/ModeNav.svelte
@@ -7,14 +7,11 @@
   .mode-container {
     padding-left: 0.5em;
     --frame-color: #dedede;
-    border-bottom: 1px solid var(--frame-color);
   }
 
   .mode-button {
-    width: 12%;
-    padding: 0.25em 0;
-    min-width: 4.2em;
-    margin-bottom: -1px;
+    padding: 0.25em 0.5em;
+    min-width: 8em;
     line-height: 1;
     box-sizing: border-box;
     color: #333;
@@ -25,38 +22,54 @@
     text-decoration: none;
     user-select: none;
 
-    transition-delay: 0s;
-    transition-duration: 0.15s;
-    transition-property: background-color;
-    transition-timing-function: ease-in-out;
-
-    border: 1px solid transparent;
-    border-bottom-color: var(--frame-color);
-    border-top-left-radius: 4px;
-    border-top-right-radius: 4px;
+    transition: backround-color 0.15s ease-in-out;
 
     background-color: #ffffff;
   }
 
-  .active,
-  .mode-button:hover,
-  .mode-button:focus,
-  .mode-button:active {
-    border: 1px solid var(--frame-color) !important;
-    border-bottom-color: #ffffff !important;
-    background-color: #ffffff !important;
+  /** deskop **/
+  @media only screen and (min-width: 768px) {
+    .mode-container {
+      border-bottom: 1px solid var(--frame-color);
+    }
+
+    .mode-button {
+      margin-bottom: -1px;
+
+      border: 1px solid transparent;
+      border-bottom-color: var(--frame-color);
+      border-top-left-radius: 4px;
+      border-top-right-radius: 4px;
+    }
+
+    .active,
+    .mode-button:hover,
+    .mode-button:focus,
+    .mode-button:active {
+      border: 1px solid var(--frame-color) !important;
+      border-bottom-color: #ffffff !important;
+      background-color: #ffffff !important;
+    }
   }
 
   @media only screen and (max-width: 767px) {
-    .hideMobile {
-      display: none;
-    }
     .mode-button {
       display: block;
       width: 100%;
       border: 0;
       text-align: left;
-      border-bottom: 1px solid var(--bg);
+      border-bottom: 1px solid var(--bg) !important;
+      margin: 0;
+    }
+    .hideMobile {
+      display: none;
+    }
+
+    .active,
+    .mode-button:hover,
+    .mode-button:focus,
+    .mode-button:active {
+      background-color: #ececec;
     }
   }
 </style>
