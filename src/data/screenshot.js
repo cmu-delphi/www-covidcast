92deb67baa674b9d61183be1f11ec6afa85bf375
Daniel LaLiberte <dlaliberte@google.com>
2020-10-08 21:31:56 -0400

    Not sure that "commit" is the right thing to do.

---
+++ src/data/screenshot.js
@@ -0,0 +1,11 @@
+export function downloadUrl(url, name) {
+  const a = document.createElement('a');
+  a.download = name;
+  a.href = url;
+  a.style.position = 'absolute';
+  a.style.left = '-10000px';
+  a.style.top = '-10000px';
+  document.body.appendChild(a);
+  a.click();
+  a.remove();
+}
