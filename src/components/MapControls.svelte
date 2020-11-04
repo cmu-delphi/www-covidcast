7146e51ba2b93c9f7b4e5277a6ce79f3f04e6427
Samuel Gratzl <sam@sgratzl.com>
2020-11-04 15:54:51 -0500

    style: migrate map control

--- src/components/MapControls.svelte
+++ src/components/MapControls.svelte
@@ -1,8 +1,5 @@
 <script>
   import { encoding, isValueSignalType } from '../stores';
-  import IoMdAdd from 'svelte-icons/io/IoMdAdd.svelte';
-  import IoMdRemove from 'svelte-icons/io/IoMdRemove.svelte';
-  import IoMdHome from 'svelte-icons/io/IoMdHome.svelte';
   import { trackEvent } from '../stores/ga';
 
   export let className = '';
@@ -26,11 +23,15 @@
     margin-bottom: 0.2em;
   }
 
+  .uk-button {
+    background: white;
+    padding: 0 4px;
+  }
+
   .encoding-button {
-    background-size: 80%;
     background-position: center;
     background-repeat: no-repeat;
-    color: transparent;
+    background-size: contain;
   }
 
   .choropleth {
@@ -50,10 +51,11 @@
   }
 </style>
 
-<div class="root base-font-size {className}">
-  <div class="pg-button-vertical-group">
+<div class="root {className}">
+  <div class="uk-vertical-button-group">
     <button
-      class="pg-button"
+      class="uk-button uk-button-default uk-button-small"
+      data-uk-icon="icon: plus"
       type="button"
       title="Zoom in"
       aria-label="Zoom in"
@@ -61,11 +63,10 @@
       on:click={() => {
         trackEvent('map', 'zoomIn');
         zoom.zoomIn();
-      }}>
-      <IoMdAdd />
-    </button>
+      }} />
     <button
-      class="pg-button"
+      class="uk-button uk-button-default uk-button-small"
+      data-uk-icon="icon: minus"
       type="button"
       title="Zoom out"
       aria-label="Zoom out"
@@ -73,58 +74,55 @@
       on:click={() => {
         trackEvent('map', 'zoomOut');
         zoom.zoomOut();
-      }}>
-      <IoMdRemove />
-    </button>
+      }} />
   </div>
   <div>
     <button
-      class="pg-button"
+      class="uk-button uk-button-default uk-button-small"
       type="button"
+      data-uk-icon="icon: home"
       title="Show entire map"
       aria-label="Show entire map"
       disabled={!zoom}
       on:click={() => {
         trackEvent('map', 'zoomReset');
         zoom.resetZoom();
-      }}>
-      <IoMdHome />
-    </button>
+      }} />
   </div>
   {#if showEncodings && $isValueSignalType}
-    <div class="pg-button-vertical-group">
+    <div class="uk-vertical-button-group">
       <button
         aria-pressed={$encoding === 'color' ? 'true' : 'false'}
-        class="pg-button encoding-button choropleth"
-        class:selected={$encoding === 'color'}
+        class="uk-button uk-button-default uk-button-small"
+        class:uk-active={$encoding === 'color'}
+        data-uk-icon="icon: dummy"
         on:click={() => {
           encoding.set('color');
         }}
         title="Switch to Choropleth">
-        <span aria-hidden>Switch to Choropleth</span>
-        <IoMdHome />
+        <svg width="20" height="20" viewBox="0 0 20 20" class="encoding-button choropleth" />
       </button>
       <button
         aria-pressed={$encoding === 'bubble' ? 'true' : 'false'}
-        class="pg-button encoding-button bubble"
-        class:selected={$encoding === 'bubble'}
+        class="uk-button uk-button-default uk-button-small"
+        class:uk-active={$encoding === 'bubble'}
+        data-uk-icon="icon: dummy"
         on:click={() => {
           encoding.set('bubble');
         }}
         title="Switch to Bubble Map">
-        <span aria-hidden>Switch to Bubble Map</span>
-        <IoMdHome />
+        <svg width="20" height="20" viewBox="0 0 20 20" class="encoding-button bubble" />
       </button>
       <button
         aria-pressed={$encoding === 'spike' ? 'true' : 'false'}
-        class="pg-button encoding-button spike"
-        class:selected={$encoding === 'spike'}
+        class="uk-button uk-button-default uk-button-small"
+        class:uk-active={$encoding === 'spike'}
+        data-uk-icon="icon: dummy"
         on:click={() => {
           encoding.set('spike');
         }}
         title="Switch to Spike Map">
-        <span aria-hidden>Switch to Spike Map</span>
-        <IoMdHome />
+        <svg width="20" height="20" viewBox="0 0 20 20" class="encoding-button spike" />
       </button>
     </div>
   {/if}
