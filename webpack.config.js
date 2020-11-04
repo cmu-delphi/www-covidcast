2414a18155dd9be3a27fd7967123ed63b2b6f44c
Samuel Gratzl <sam@sgratzl.com>
2020-11-04 09:47:26 -0500

    style: strip old styles

--- webpack.config.js
+++ webpack.config.js
@@ -137,11 +137,6 @@ module.exports = () => {
         alwaysWriteToDisk: true,
         template: './src/index.html',
       }),
-      new HtmlWebpackPlugin({
-        filename: 'embed.html',
-        alwaysWriteToDisk: true,
-        template: './src/embed.html',
-      }),
       new HtmlWebpackHarddiskPlugin(),
       new MiniCssExtractPlugin({
         filename: '[name].css',
