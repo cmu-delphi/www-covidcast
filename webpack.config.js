8dc61bd82b952d7704052ca6205bf460a2da2c0c
Samuel Gratzl <sam@sgratzl.com>
2020-11-12 15:57:49 -0500

    build: use prime url during netlify build

--- webpack.config.js
+++ webpack.config.js
@@ -24,7 +24,7 @@ module.exports = () => {
       path: path.resolve(__dirname, 'public'),
       filename: '[name].js',
       chunkFilename: '[name].js',
-      // publicPath: './',
+      publicPath: process.env.DEPLOY_PRIME_URL ? process.env.DEPLOY_PRIME_URL : undefined,
     },
 
     resolve: {
