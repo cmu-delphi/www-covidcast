ceb45dd0c6e290c01d575d3f6e46e669e03fd36e
Samuel Gratzl <sam@sgratzl.com>
2020-11-16 15:21:50 -0500

    build: remove public path

--- webpack.config.js
+++ webpack.config.js
@@ -24,7 +24,7 @@ module.exports = () => {
       path: path.resolve(__dirname, 'public'),
       filename: '[name].js',
       chunkFilename: '[name].js',
-      publicPath: process.env.DEPLOY_PRIME_URL ? `${process.env.DEPLOY_PRIME_URL}/` : undefined,
+      // publicPath: process.env.DEPLOY_PRIME_URL ? `${process.env.DEPLOY_PRIME_URL}/` : undefined,
     },
 
     resolve: {
