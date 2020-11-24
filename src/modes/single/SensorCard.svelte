dae5506d2efcce1c4ae595a31c20aede049a9575
Daniel LaLiberte <dlaliberte@google.com>
2020-11-24 13:16:34 -0500

    Fix legend color for first signal (to be gray) in Region Details.

--- src/modes/single/SensorCard.svelte
+++ src/modes/single/SensorCard.svelte
@@ -145,7 +145,7 @@
     <tbody>
       {#each selections as selection, i}
         <tr>
-          <td class="legend" style="--color: {selection.color}">{selection.displayName}</td>
+          <td class="legend" style="--color: {i === 0 ? 'grey' : selection.color}">{selection.displayName}</td>
           <td class="key-fact">{values[i] != null ? sensor.formatValue(values[i]) : '?'}</td>
           {#if i === 0}
             <td class="hint" rowspan={selections.length}>on {formatLocal(highlightDate ? highlightDate : date)}</td>
