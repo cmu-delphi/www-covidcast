c7712e2af92dcb5b1b93a44b6023fead47b4ff7d
Samuel Gratzl <sam@sgratzl.com>
2021-01-20 14:33:20 +0100

    refactor: remove dead code

--- src/util.js
+++ src/util.js
@@ -85,3 +85,15 @@ export function unobserveResize(element) {
   observerListeners.delete(element);
   observer.unobserve(element);
 }
+
+export function downloadUrl(url, name) {
+  const a = document.createElement('a');
+  a.download = name;
+  a.href = url;
+  a.style.position = 'absolute';
+  a.style.left = '-10000px';
+  a.style.top = '-10000px';
+  document.body.appendChild(a);
+  a.click();
+  a.remove();
+}
