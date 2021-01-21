b499078ed1da4f3ff5ec2bde53c4b7b93c7ea30f
Samuel Gratzl <sam@sgratzl.com>
2021-01-21 11:10:31 +0100

    feat: conditional padding for chart

--- src/modes/lab/DataJournalistChart.svelte
+++ src/modes/lab/DataJournalistChart.svelte
@@ -1,8 +1,10 @@
 <script>
   import { timeDay } from 'd3-time';
+import Tooltip from '../../components/MapBox/Tooltip.svelte';
   import Vega from '../../components/Vega.svelte';
   import { addMissing, fetchTimeSlice } from '../../data';
   import { defaultRegionOnStartup, sensorList } from '../../stores/constants';
+  import {sequenceGen} from './seq';
 
   const sensor = sensorList.find((d) => d.isCasesOrDeath);
   /**
@@ -20,10 +22,6 @@
     data: {
       name: 'values',
     },
-    mark: {
-      type: 'line',
-      pont: false,
-    },
     encoding: {
       x: {
         field: 'date_value',
@@ -34,9 +32,9 @@
           format: '%b %d',
           formatType: 'time',
           labelFontSize: 14,
-          tickCount: {
-            interval: 'day',
-          },
+          // tickCount: {
+          //   interval: 'day',
+          // },
         },
         scale: {},
       },
@@ -53,11 +51,31 @@
         scale: {
           round: true,
           zero: false,
-          // domainMin: 0,
-          // padding: 50,
+          domainMin: null,
+          padding: {
+            // in case the values are close to 0 .. no padding otherwise some padding
+            // if range.min < 10 && range.range > 30 ? 0 : 20
+            expr: `customObjChecks(customExtent(data("values"), "value"), ['min', '<', 10], ['range', '>', 30]) ? 0 : 20`
+          },
         },
       },
     },
+    layer: [
+      {
+        mark: {
+          type: 'line',
+          point: false,
+        },
+      },
+      {
+        mark: {
+          type: 'point',
+          stroke: null,
+          fill: 'steelblue',
+          tooltip: true,
+        },
+      },
+    ],
     config: {
       view: {
         stroke: null,
@@ -74,19 +92,23 @@
     },
   };
 
-  function rnd(min, max) {
-    const v = Math.random();
-    return min + v * (max - min);
-  }
-
-  function gen() {
+  function gen(min, max, seed) {
+    const seq = sequenceGen(min, max, seed);
     return timeDay.range(new Date(2020, 12 - 1, 1), new Date(), 1).map((date_value) => ({
       date_value,
-      value: rnd(840, 880),
+      value: seq(),
     }));
   }
 
-  const data = gen();
+  // const data = fetchTimeSlice(sensor, 'county', defaultRegionOnStartup.county, new Date(2020, 12 - 1, 1), new Date()).then((r) =>
+  //   addMissing(r, sensor),
+  // );
+
+  const dataWide = gen(0, 100);
+  const dataWide2 = gen(0, 100, "x");
+  const dataLowBand = gen(3, 8);
+  const dataMidBand = gen(30, 60);
+  const dataHighBand = gen(87, 92);
 </script>
 
 <style>
@@ -94,4 +116,9 @@
 
 <h2>Data Journalist Chart</h2>
 
-<Vega {spec} {data} />
+<Vega {spec} data={dataWide} />
+<Vega {spec} data={dataWide2} />
+<Vega {spec} data={dataLowBand} />
+<Vega {spec} data={dataMidBand} />
+<Vega {spec} data={dataHighBand} />
+
