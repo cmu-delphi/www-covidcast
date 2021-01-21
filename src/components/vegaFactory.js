a56ccf5bf880f81c85752747434dcb5594a41e29
Samuel Gratzl <sam@sgratzl.com>
2021-01-21 11:10:09 +0100

    feat: add custom vega functions
    for advanced ifs and computations

--- src/components/vegaFactory.js
+++ src/components/vegaFactory.js
@@ -41,3 +41,79 @@ export default function createVega(root, spec, options) {
     ...options,
   });
 }
+
+/**
+ * 
+ * @param {any[]} array 
+ * @param {string} field 
+ */
+function customMap(array, field) {
+  return array.map((d) => d != null ? d[field] : null);
+}
+
+/**
+ * 
+ * @param {any[]} array 
+ * @param {string} field 
+ */
+function customFilter(array, field, value) {
+  return array.filter((d) => d != null && d[field] === value);
+}
+
+/**
+ * 
+ * @param {any[]} array 
+ * @param {string} field 
+ */
+function customReduce(array, field, reduceFunction, initialValue) {
+  return array.reduce((acc, d) => reduceFunction(acc, d != null ? d[field] : null), initialValue);
+}
+
+expressionFunction('customMap', customMap);
+expressionFunction('customFilter', customFilter);
+expressionFunction('customReduce', customReduce);
+
+function customExtent(arr, field) {
+  if (arr.length === 0) {
+    return {
+      min: null,
+      max: null,
+      range: 0
+    };
+  }
+  let min = Number.POSITIVE_INFINITY;
+  let max = Number.NEGATIVE_INFINITY;
+
+  for (const d of arr) {
+    const v = d != null ? d[field] : null;
+    if (v != null) {
+      min = Math.min(min, +v);
+      max = Math.max(max, +v);
+    }
+  }
+
+  return {
+    min, max, range: max - min,
+  };
+}
+expressionFunction('customExtent', customExtent);
+
+function customObjChecks(obj, ...conditions) {
+  if (conditions.length === 0) {
+    return true;
+  }
+  return conditions.every(([field, op, value]) => {
+    const v = obj != null ? obj[field] : null;
+    switch (op) {
+      case '==': return v == value;
+      case '<': return v < value;
+      case '>': return v > value;
+      case '>=': return v >= value;
+      case '<=': return v <= value;
+      default:
+        return v === value;
+    }
+  });
+}
+
+expressionFunction('customObjChecks', customObjChecks);
