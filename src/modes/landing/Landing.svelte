9a592047bc498d84026ce29d5c71097ef011aa97
Samuel Gratzl <sam@sgratzl.com>
2021-02-17 16:43:33 +0100

    feat(landing): add indicator number summary

--- src/modes/landing/Landing.svelte
+++ src/modes/landing/Landing.svelte
@@ -1,10 +1,11 @@
 <script>
   import Search from '../../components/Search.svelte';
   import { countyInfo, stateInfo } from '../../maps';
-  import { currentMode, recentRegionInfos, selectByInfo } from '../../stores';
+  import { currentMode, groupedSensorList, recentRegionInfos, selectByInfo } from '../../stores';
   import flagUSAIcon from '!raw-loader!@fortawesome/fontawesome-free/svgs/solid/flag-usa.svg';
   import arrowRightIcon from '!raw-loader!@fortawesome/fontawesome-free/svgs/solid/arrow-circle-right.svg';
   import { modeByID } from '..';
+  import { questions } from '../survey/questions';
 
   function switchMode(region) {
     if (region !== undefined) {
@@ -50,15 +51,19 @@
   }
 
   .highlights {
-    vertical-align: end;
+    vertical-align: bottom;
   }
+
   .highlights th {
     text-align: right;
-    margin-right: 1em;
+    padding: 0.5em 1em;
   }
+
   .highlights h3 {
     font-size: 1.5rem;
     font-weight: 600;
+    line-height: 1.5rem;
+    margin: 0;
   }
 
   .chips {
@@ -81,78 +86,77 @@
   }
 </style>
 
-<div class="uk-container root">
-  <h2>Welcome to COVIDcast</h2>
-  <p>Explore COVID-19 indicators nearby</p>
+<div class="uk-container content-grid root">
+  <div class="grid-3-11">
+    <h2>Welcome to COVIDcast</h2>
+    <p>Explore COVID-19 indicators nearby</p>
 
-  <Search
-    modern
-    placeholder="Search for state or county"
-    items={stateInfo.concat(countyInfo)}
-    labelFieldName="displayName"
-    maxItemsToShowInList="5"
-    on:change={(e) => selectByInfo(e.detail && e.detail.level === 'nation' ? null : e.detail)} />
+    <Search
+      modern
+      placeholder="Search for state or county"
+      items={stateInfo.concat(countyInfo)}
+      labelFieldName="displayName"
+      maxItemsToShowInList="5"
+      on:change={(e) => selectByInfo(e.detail && e.detail.level === 'nation' ? null : e.detail)} />
 
-  <div class="chips">
-    {#each $recentRegionInfos.slice(0, 3) as region}
-      <a
-        class="chip uk-link-text uk-link-toggle"
-        href="?mode=mobile&region={region.propertyId}"
-        on:click|preventDefault={() => switchMode(region)}>
-        {region.displayName}
-      </a>
-    {/each}
-  </div>
-  <div class="summary-button-bar">
-    <div>
-      <a
-        class="uk-button uk-button-default uk-button-delphi"
-        href="?mode=mobile"
-        on:click|preventDefault={() => switchMode()}>
-        <span class="inline-svg-icon">
-          {@html arrowRightIcon}
-        </span>
-        Continue
-      </a>
+    <div class="chips">
+      {#each $recentRegionInfos.slice(0, 3) as region}
+        <a
+          class="chip uk-link-text uk-link-toggle"
+          href="?mode=mobile&region={region.propertyId}"
+          on:click|preventDefault={() => switchMode(region)}>
+          {region.displayName}
+        </a>
+      {/each}
     </div>
-    <div>
-      <a
-        class="uk-button uk-button-default uk-button-delphi"
-        href="?mode=mobile"
-        on:click|preventDefault={() => switchMode(null)}>
-        <span class="inline-svg-icon">
-          {@html flagUSAIcon}
-        </span>
-        View United States
-      </a>
+    <div class="summary-button-bar">
+      <div>
+        <a
+          class="uk-button uk-button-default uk-button-delphi"
+          href="?mode=mobile"
+          on:click|preventDefault={() => switchMode()}>
+          <span class="inline-svg-icon">
+            {@html arrowRightIcon}
+          </span>
+          Continue
+        </a>
+      </div>
+      <div>
+        <a
+          class="uk-button uk-button-default uk-button-delphi"
+          href="?mode=mobile"
+          on:click|preventDefault={() => switchMode(null)}>
+          <span class="inline-svg-icon">
+            {@html flagUSAIcon}
+          </span>
+          View United States
+        </a>
+      </div>
     </div>
+
+    <p>
+      COVIDcast provides real-time county-level indicators in the United States. Our indicators include our national
+      COVID survey, public experience, attitude, behavior, doctor's visits, hospital admissions, tests, cases and
+      deaths.
+    </p>
+
+    <table class="highlights">
+      <tr>
+        <th>
+          <h3>{questions.length}</h3>
+        </th>
+        <td>Survey Indicators</td>
+      </tr>
+      {#each groupedSensorList as group}
+        <tr>
+          <th>
+            <h3>{group.sensors.length}</h3>
+          </th>
+          <td>{group.label}{group.label.endsWith('Indicators') ? '' : ' Indicators'}</td>
+        </tr>
+      {/each}
+    </table>
+
+    <p>In collaboration with Facebook, Google, Change Healthcare, and Quidel.</p>
   </div>
-
-  <p>
-    Real-time county level indicators for COVID-19. Our indicators include public experience, attitude, behavior,
-    doctors visits, hospital admissions, tests, cases, hospitalizations, and deaths.
-  </p>
-
-  <table class="highlights">
-    <tr>
-      <th>
-        <h3>TODO</h3>
-      </th>
-      <td>Total survey responses</td>
-    </tr>
-    <tr>
-      <th>
-        <h3>TODO</h3>
-      </th>
-      <td>Forecasting Models</td>
-    </tr>
-    <tr>
-      <th>
-        <h3>TODO</h3>
-      </th>
-      <td>Origianl COVID-19 indicators</td>
-    </tr>
-  </table>
-
-  <p>In collaboration with Facebook, Google, Change Healthcare, and Quidel.</p>
 </div>
