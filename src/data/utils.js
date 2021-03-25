2150cbc2bc640ee9b99e0f9893d24f381b5a270a
Chris Scott <chrissco@andrew.cmu.edu>
2021-03-25 15:52:24 -0400

    Add r2 at lag 0 functionality.

--- src/data/utils.js
+++ src/data/utils.js
@@ -2,6 +2,7 @@
 
 import { timeDay } from 'd3-time';
 import { timeParse, timeFormat } from 'd3-time-format';
+import { linear } from 'regression';
 
 // json1 value is 7 day average, json2 value is single count
 /**
@@ -38,3 +39,37 @@ export function parseAPITime(v) {
  * @type {(v: Date) => string}
  */
 export const formatAPITime = timeFormat('%Y%m%d');
+
+/**
+ * @typedef {object} CorrelationMetric
+ * @property {number} r2At0
+ * @property {number} lagAtMaxR2
+ * @property {number} r2AtMaxR2
+ */
+
+/**
+ * @param {import('./fetchData').EpiDataRow[]} response
+ * @param {import('./fetchData').EpiDataRow[]} explanatory
+ * @param {CorrelationMetric} metric
+ */
+export function generateCorrelationMetrics(response, explanatory) {
+  let response_values = response.map((row) => {
+    return row.value;
+  });
+
+  let explanatory_values = explanatory.map((row) => {
+    return row.value;
+  });
+
+  let zipped_values = response_values.map((x, i) => {
+    return [explanatory_values[i], x];
+  });
+  console.log(zipped_values);
+  let model = linear(zipped_values);
+  console.log(JSON.stringify(model));
+  return {
+    r2At0: model.r2,
+    lagAtMaxR2: 0,
+    r2AtMaxR2: model.r2,
+  };
+}
--- src/data/utils.spec.js
+++ src/data/utils.spec.js
@@ -1,4 +1,9 @@
-import { formatAPITime, parseAPITime } from './utils';
+import { formatAPITime, parseAPITime, generateCorrelationMetrics } from './utils';
+import { callAPIEndPoint } from './api';
+import { enableFetchMocks } from 'jest-fetch-mock';
+import fetchMocks from 'jest-fetch-mock';
+
+enableFetchMocks();
 
 describe('formatAPITime', () => {
   test('matches api format', () => {
@@ -20,5 +25,34 @@ describe('parseAPITime', () => {
     const d = new Date(2020, 5, 8, 5, 2);
     const day = new Date(2020, 5, 8);
     expect(parseAPITime(formatAPITime(d)).valueOf()).toBe(day.valueOf());
+  });
+});
+
+describe('correlationMetrics', () => {
+  test('simple', async () => {
+    fetchMocks.dontMock();
+    let deaths = await callAPIEndPoint(
+      'https://api.covidcast.cmu.edu/epidata/api.php',
+      'indicator-combination',
+      'deaths_7dav_incidence_prop',
+      'county',
+      [new Date('2020-10-30'), new Date('2021-03-21')],
+      '42003',
+    );
+    let cli = await callAPIEndPoint(
+      'https://api.covidcast.cmu.edu/epidata/api.php',
+      'fb-survey',
+      'smoothed_hh_cmnty_cli',
+      'county',
+      [new Date('2020-10-30'), new Date('2021-03-21')],
+      '42003',
+    );
+
+    let metrics = {
+      r2At0: 0.43,
+      lagAtMaxR2: 0,
+      r2AtMaxR2: 0.43,
+    };
+    expect(generateCorrelationMetrics(deaths.epidata, cli.epidata)).toEqual(metrics);
   });
 });
