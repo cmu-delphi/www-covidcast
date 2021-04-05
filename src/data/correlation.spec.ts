9049f5e777bf60efde270e9acee4f76d7539f2d2
Samuel Gratzl <sam@sgratzl.com>
2021-04-05 09:03:59 -0400

    refactor: correlation

--- src/data/correlation.spec.ts
+++ src/data/correlation.spec.ts
@@ -1,32 +1,33 @@
+/// <reference types="jest" />
 import { generateCorrelationMetrics } from './correlation';
 import { cliTestData, deathsTestData, cases_national, safegraph_full_time_national } from './__test__/testData';
 
 describe('correlationMetrics', () => {
-  test('Deaths vs. CLI correlations should equal what was published in the blog post', async () => {
-    let expected_metrics = {
+  test('Deaths vs. CLI correlations should equal what was published in the blog post', () => {
+    const expected_metrics = {
       r2At0: 0.5,
       lagAtMaxR2: 20,
       r2AtMaxR2: 0.81,
     };
-    let actual_metrics = generateCorrelationMetrics(deathsTestData, cliTestData);
+    const actual_metrics = generateCorrelationMetrics(deathsTestData, cliTestData);
     expect(actual_metrics.r2At0).toEqual(expected_metrics.r2At0);
     expect(actual_metrics.lagAtMaxR2).toEqual(expected_metrics.lagAtMaxR2);
     expect(actual_metrics.r2AtMaxR2).toEqual(expected_metrics.r2AtMaxR2);
     expect(actual_metrics.lags.length).toEqual(28 * 2 + 1);
   });
-  test('Signals with different date ranges should correlate on the union of their dates.', async () => {
-    let expected_metrics = {
+  test('Signals with different date ranges should correlate on the union of their dates.', () => {
+    const expected_metrics = {
       r2At0: -0.59,
       lagAtMaxR2: 28,
       r2AtMaxR2: -0.07,
     };
-    let actual_metrics = generateCorrelationMetrics(cases_national, safegraph_full_time_national);
+    const actual_metrics = generateCorrelationMetrics(cases_national, safegraph_full_time_national);
     expect(actual_metrics.r2At0).toEqual(expected_metrics.r2At0);
     expect(actual_metrics.lagAtMaxR2).toEqual(expected_metrics.lagAtMaxR2);
     expect(actual_metrics.r2AtMaxR2).toEqual(expected_metrics.r2AtMaxR2);
     expect(actual_metrics.lags.length).toEqual(28 * 2 + 1);
   });
-  test('Signals with not enough overlap should error.', async () => {
+  test('Signals with not enough overlap should error.', () => {
     expect(() => {
       generateCorrelationMetrics(cases_national, safegraph_full_time_national.slice(-30));
     }).toThrow();
--- src/data/correlation.ts
+++ src/data/correlation.ts
@@ -1,40 +1,44 @@
-import { linear } from 'regression';
+import { linear, Result } from 'regression';
 import { zip } from '../util';
+import type { EpiDataRow } from './fetchData';
 
-/**
- * @typedef {object} Lag
- * @property {number} lag
- * @property {number} r2
- * @property {number} slope y = slope * x + intercept
- * @property {number} intercept y = slope * x + intercept
- * @property {number} samples number of dates used for the regression line
- * @property {import('./fetchData').EpiDataRow[]} a
- * @property {import('./fetchData').EpiDataRow[]} b
- */
+export interface Lag<T = EpiDataRow> {
+  lag: number;
+  r2: number;
 
-/**
- * @typedef {object} CorrelationMetric
- * @property {number} r2At0
- * @property {number} lagAtMaxR2
- * @property {number} r2AtMaxR2
- * @property {Lag} t0
- * @property {Lag} tMax
- * @property {Lag[]} lags
- */
+  /**
+   * y = slope * x + intercept
+   */
+  slope: number;
+  /**
+   * y = slope * x + intercept
+   */
+  intercept: number;
+
+  /**
+   * number of dates used for the regression line
+   */
+  samples: number;
+
+  a: T[];
+  b: T[];
+}
+
+export interface CorrelationMetric<T = EpiDataRow> {
+  r2At0: number;
+  lagAtMaxR2: number;
+  r2AtMaxR2: number;
+  t0?: Lag<T>;
+  tMax?: Lag<T>;
+  lags: Lag<T>[];
+}
 
 /**
  * Use this many days of lag to compute correlation metrics
- * @type number
  */
 const lag = 28;
 
-/**
- *
- * @param {number} lag
- * @param {import('regression').Result} model
- * @return {Lag}
- */
-function asLag(lag, model, a, b) {
+function asLag<T>(lag: number, model: Result, a: T[], b: T[]): Lag<T> {
   return {
     r2: model.r2,
     lag,
@@ -45,6 +49,7 @@ function asLag(lag, model, a, b) {
     b,
   };
 }
+
 /**
  * Generates R^2 metrics for lags between -28 and 28 days.
  *
@@ -54,16 +59,9 @@ function asLag(lag, model, a, b) {
  * For each lag, the input is a window of length(a)-28, such that the number of values at
  * each lag is the same.
  *
- * @template {{value: number}} T
- * @param {T[]} a
- * @param {T[]} b
- * @returns {Lag[]}
  */
-function generateLags(a, b) {
-  /**
-   * @type {Lag[]}
-   */
-  const lags = [];
+function generateLags<T extends { value: number }>(a: readonly T[], b: readonly T[]): Lag<T>[] {
+  const lags: Lag<T>[] = [];
 
   const aValues = a.map((d) => d.value);
   const bValues = b.map((d) => d.value);
@@ -91,18 +89,17 @@ function generateLags(a, b) {
 
 /**
  * Do a pair-wise intersection of EpiDataRow by date.
- * @template {{time_value: number}} T
- * @param {T[]} a
- * @param {T[]} b
- * @returns {[T, T][]}
  */
-function intersectEpiDataRow(a, b) {
+function intersectEpiDataRow<T extends { time_value: number; value: number }>(
+  a: readonly T[],
+  b: readonly T[],
+): [T, T][] {
   const aLength = a.length;
   const bLength = b.length;
   let aIndex = 0;
   let bIndex = 0;
 
-  const intersection = [];
+  const intersection: [T, T][] = [];
 
   while (aIndex < aLength && bIndex < bLength) {
     if (a[aIndex].time_value < b[bIndex].time_value) {
@@ -122,11 +119,11 @@ function intersectEpiDataRow(a, b) {
 /**
  * Compute 28-day correlation metrics for a response variable given an explanatory variable.
  *
- * @param {import('./fetchData').EpiDataRow[]} response
- * @param {import('./fetchData').EpiDataRow[]} explanatory
- * @returns {CorrelationMetric}
  */
-export function generateCorrelationMetrics(response, explanatory) {
+export function generateCorrelationMetrics<T extends { time_value: number; value: number }>(
+  response: readonly T[],
+  explanatory: readonly T[],
+): CorrelationMetric<T> {
   const zippedEpiData = intersectEpiDataRow(response, explanatory);
   if (zippedEpiData.length < lag * 2) {
     throw new Error(
