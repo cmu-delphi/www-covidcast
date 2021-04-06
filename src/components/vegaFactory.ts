81c1a748f3e45b25ec52017594b3eb84863c3f37
Samuel Gratzl <sam@sgratzl.com>
2021-04-06 10:33:02 -0400

    refactor: migrate vegaFactory

--- src/components/vegaFactory.ts
+++ src/components/vegaFactory.ts
@@ -1,14 +1,16 @@
 import { timeFormat } from 'd3-time-format';
 import { format } from 'd3-format';
-import embed from 'vega-embed';
+import embed, { EmbedOptions, Result, VisualizationSpec } from 'vega-embed';
 import { Error, expressionFunction, projection } from 'vega';
 import { geoAlbersUsaTerritories } from 'geo-albers-usa-territories';
-import { fitExtent, fitSize, fitWidth, fitHeight } from 'd3-geo/src/projection/fit.js';
+import type { GeoProjection } from 'd3-geo';
+import { fitExtent, fitSize, fitWidth, fitHeight } from 'd3-geo/src/projection/fit';
 import { timeDay } from 'd3-time';
 
-function patchedAlbersUsaTerritories() {
+function patchedAlbersUsaTerritories(): GeoProjection {
   // see https://github.com/stamen/geo-albers-usa-territories/pull/8/files
-  const r = geoAlbersUsaTerritories();
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+  const r: GeoProjection = geoAlbersUsaTerritories();
   r.fitExtent = function (extent, object) {
     return fitExtent(r, extent, object);
   };
@@ -29,9 +31,9 @@ function patchedAlbersUsaTerritories() {
 
 projection('albersUsaTerritories', patchedAlbersUsaTerritories);
 
-const cache = new Map();
+const cache = new Map<string, (d: Date | number) => string>();
 
-export function cachedTime(datum, params) {
+export function cachedTime(datum: Date, params: string): string {
   const key = `d:${params}`;
   if (cache.has(key)) {
     return cache.get(key)(datum);
@@ -41,7 +43,7 @@ export function cachedTime(datum, params) {
   return formatter(datum);
 }
 
-export function cachedNumber(datum, params) {
+export function cachedNumber(datum: number, params: string): string {
   const key = `n:${params}`;
   if (cache.has(key)) {
     return cache.get(key)(datum);
@@ -54,13 +56,11 @@ export function cachedNumber(datum, params) {
 expressionFunction('cachedTime', cachedTime);
 expressionFunction('cachedNumber', cachedNumber);
 
-/**
- * @param {string | HTMLElement} root
- * @param {import('vega-embed').VisualizationSpec | string} spec
- * @param {import('vega-embed').EmbedOptions} options
- * @return {Promise<import('vega-embed').Result>}
- */
-export default function createVega(root, spec, options) {
+export default function createVega(
+  root: string | HTMLElement,
+  spec: VisualizationSpec | string,
+  options: EmbedOptions,
+): Promise<Result> {
   return embed(root, spec, {
     actions: false,
     logLevel: Error,
@@ -68,7 +68,7 @@ export default function createVega(root, spec, options) {
   });
 }
 
-function customExtent(arr, field) {
+function customExtent(arr: Record<string, number>[], field: string): { min?: number; max?: number; range: number } {
   if (arr.length === 0) {
     return {
       min: null,
@@ -95,7 +95,10 @@ function customExtent(arr, field) {
 }
 expressionFunction('customExtent', customExtent);
 
-function customObjChecks(obj, ...conditions) {
+function customObjChecks(
+  obj: Record<string, unknown>,
+  ...conditions: readonly [string, '==' | '<' | '>' | '<=' | '>=' | '===', number | string][]
+): boolean {
   if (conditions.length === 0) {
     return true;
   }
@@ -122,14 +125,17 @@ expressionFunction('customObjChecks', customObjChecks);
 
 /**
  * patches the given event and updates the picked item if needed
- * @param {MouseEvent} event
  */
-function patchPickedItem(event) {
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+function patchPickedItem(event: TouchEvent & { item: unknown; vega: { view(): any; item: unknown } }) {
   if (event.type === 'touchmove' || event.type === 'touchend') {
     // manually resolve, see https://github.com/vega/vega/issues/3065
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
     event.item = event.vega.view()._handler.pickEvent(event.changedTouches[0]);
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-return
     event.vega.item = () => event.item;
   }
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-return
   return event.item;
 }
 expressionFunction('patchPickedItem', patchPickedItem);
@@ -137,24 +143,17 @@ expressionFunction('patchPickedItem', patchPickedItem);
 /**
  * computes padding such that the chart are is centered and squared for the given parameters
  * @param {[number, number]} container size
- * @param {any[]} x scale domain
- * @param {any[]} domainY
- * @param {{left: number, top: number, right: number, bottom: number}} basePadding
- * @param {number} paddingXOuter
- * @param {number} paddingYOuter
- * @param {number} factorX
- * @param {number} factorY
  */
 function paddingSquareCenter(
-  [width, height],
-  domainX,
-  domainY,
-  basePadding,
-  paddingXOuter,
-  paddingYOuter,
-  factorX,
-  factorY,
-) {
+  [width, height]: [number, number],
+  domainX: unknown[],
+  domainY: unknown[],
+  basePadding: { left: number; top: number; right: number; bottom: number },
+  paddingXOuter: number,
+  paddingYOuter: number,
+  factorX: number,
+  factorY: number,
+): { left: number; top: number; right: number; bottom: number } {
   const wBase = width - basePadding.left - basePadding.right;
   const hBase = height - basePadding.top - basePadding.bottom;
   const domainXW = domainX.length + paddingXOuter * 2;
@@ -174,15 +173,17 @@ function paddingSquareCenter(
 }
 expressionFunction('paddingSquareCenter', paddingSquareCenter);
 
-function array2object(arr, key) {
-  const r = {};
+function array2object<T extends Record<string, unknown>>(arr: T[], key: string): Record<string, T> {
+  const r: Record<string, T> = {};
   for (const v of arr) {
-    r[v[key]] = v;
+    r[String(v[key])] = v;
   }
   return r;
 }
 expressionFunction('array2object', array2object);
 
-expressionFunction('customInFilter', (arr, prop, values) => arr.filter((d) => values.includes(d[prop])));
+expressionFunction('customInFilter', <T>(arr: Record<string, T>[], prop: string, values: T[]) =>
+  arr.filter((d) => values.includes(d[prop])),
+);
 
-expressionFunction('customCountDays', (d1, d2) => timeDay.count(d1, d2));
+expressionFunction('customCountDays', (d1: Date, d2: Date) => timeDay.count(d1, d2));
