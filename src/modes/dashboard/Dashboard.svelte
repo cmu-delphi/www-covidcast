161d7e2e3b446829fb34d64d9bf89bc50d0aa35c
Samuel Gratzl <sam@sgratzl.com>
2021-04-13 10:57:08 -0400

    feat: prepare dashboard mode

---
+++ src/modes/dashboard/Dashboard.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  import '../mobile/common.css';
+</script>
+
+<div class="root">
+  <div class="mobile-header-line-bg">
+    <div class="mobile-header-line">
+      <h2>COVIDcast <span>Dashboard</span></h2>
+    </div>
+  </div>
+  <div class="panel">TODO</div>
+</div>
+
+<style>
+  .root {
+    position: relative;
+    flex: 1 1 0;
+    font-size: 0.875rem;
+    line-height: 1.5rem;
+  }
+</style>
