871dc93c36233fa3c2b9f1d10b1778d206905923
Samuel Gratzl <sam@sgratzl.com>
2021-04-23 16:39:20 -0400

    fix: allow default level

--- src/modes/dashboard/config/LevelPicker.svelte
+++ src/modes/dashboard/config/LevelPicker.svelte
@@ -11,7 +11,7 @@
 
 <div>
   <label for="widget-adder-l" class="uk-form-label">Geographic Level</label>
-  <select id="widget-adder-l" class="uk-select" name="level" required>
+  <select id="widget-adder-l" class="uk-select" name="level">
     <option value="">Default ({getLevelInfo(shownLevel).label})</option>
     {#each levelList.slice(1) as level}
       <option value={level.id}>{level.label}</option>
