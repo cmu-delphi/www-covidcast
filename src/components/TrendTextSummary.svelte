964501ef3848f8ba7f65cf2c41a67ccf6a81a2f2
Samuel Gratzl <sam@sgratzl.com>
2021-04-29 10:17:32 -0400

    fix: tune trend text for neutral

--- src/components/TrendTextSummary.svelte
+++ src/components/TrendTextSummary.svelte
@@ -38,38 +38,72 @@
     {#if d.isUnknown}
       We don't have data on the historical context for this indicator on
       {formatDateWeekday(date.value, true)}.
-    {:else if d.isNeutral && +date.value === +d.minDate}
-      On
-      {formatDateWeekday(date.value, true)}
-      <strong>{sensor.value.name}</strong>
-      has
-      <strong>
-        <TrendText trend={d.maxTrend} />
-      </strong>
-      compared to the
-      <strong
-        >{WINDOW_SIZE}
-        month maximum value of
-        <SensorValue {sensor} value={d.max ? d.max.value : null} medium /></strong
-      >
-      on
-      <strong>{formatDateYearWeekdayAbbr(d.maxDate, true)}</strong>.
-    {:else if d.isNeutral}
-      On
-      {formatDateWeekday(date.value, true)}
-      <strong>{sensor.value.name}</strong>
-      has
-      <strong>
-        <TrendText trend={d.minTrend} />
-      </strong>
-      compared to the
-      <strong
-        >{WINDOW_SIZE}
-        month minimum value of
-        <SensorValue {sensor} value={d.min ? d.min.value : null} medium /></strong
-      >
-      on
-      <strong>{formatDateYearWeekdayAbbr(d.minDate, true)}</strong>.
+    {:else if d.highValuesAre === 'neutral'}
+      {#if d.isNeutral}
+        On
+        {formatDateWeekday(date.value, true)}
+        <strong>{sensor.value.name}</strong>
+        has
+        <strong>
+          <TrendText trend={d.minTrend} />
+        </strong>
+        compared to the
+        <strong
+          >{WINDOW_SIZE}
+          month minimum value of
+          <SensorValue {sensor} value={d.min ? d.min.value : null} medium /></strong
+        >
+        on
+        <strong>{formatDateYearWeekdayAbbr(d.minDate, true)}</strong>.
+      {:else if +date.value === +d.minDate}
+        On
+        {formatDateWeekday(date.value, true)}
+        <strong>{sensor.value.name}</strong>
+        has
+        <strong>
+          <TrendText trend={d.maxTrend} />
+        </strong>
+        compared to the
+        <strong
+          >{WINDOW_SIZE}
+          month maximum value of
+          <SensorValue {sensor} value={d.max ? d.max.value : null} medium /></strong
+        >
+        on
+        <strong>{formatDateYearWeekdayAbbr(d.maxDate, true)}</strong>.
+      {:else if +date.value === +d.maxDate}
+        On
+        {formatDateWeekday(date.value, true)}
+        <strong>{sensor.value.name}</strong>
+        has
+        <strong>
+          <TrendText trend={d.minTrend} />
+        </strong>
+        compared to the
+        <strong
+          >{WINDOW_SIZE}
+          month minimum value of
+          <SensorValue {sensor} value={d.min ? d.min.value : null} medium /></strong
+        >
+        on
+        <strong>{formatDateYearWeekdayAbbr(d.minDate, true)}</strong>.
+      {:else}
+        On
+        {formatDateWeekday(date.value, true)}
+        <strong>{sensor.value.name}</strong>
+        has
+        <strong>
+          <TrendText trend={d.minTrend} />
+        </strong>
+        compared to the
+        <strong
+          >{WINDOW_SIZE}
+          month minimum value of
+          <SensorValue {sensor} value={d.min ? d.min.value : null} medium /></strong
+        >
+        on
+        <strong>{formatDateYearWeekdayAbbr(d.minDate, true)}</strong>.
+      {/if}
     {:else if +date.value === +d.worstDate}
       On
       {formatDateWeekday(date.value, true)}
