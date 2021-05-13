3345ed6b4eff85267d1b3b510e19fc604a6171a5
Samuel Gratzl <sam@sgratzl.com>
2021-05-13 08:42:03 -0400

    fix: remove old attribute

--- src/modes/dashboard/DashboardParameters.svelte
+++ src/modes/dashboard/DashboardParameters.svelte
@@ -53,7 +53,7 @@
     maxItemsToShowInList="5"
     on:change={(e) => region.set(e.detail && e.detail.level === 'nation' ? null : e.detail)}
   />
-  <SensorDatePicker2 className="grid-9-13" bind:value={selectedDate} {sensor} level={region.value.level} />
+  <SensorDatePicker2 className="grid-9-13" bind:value={selectedDate} {sensor} />
 </div>
 
 <style>
