933b063193fac6fd9cb585e7637e5a786db3fbc2
Samuel Gratzl <sam@sgratzl.com>
2021-08-19 10:26:34 -0400

    feat: sort sourcres by source

--- src/data/meta.ts
+++ src/data/meta.ts
@@ -163,68 +163,70 @@ function deriveMetaSensors(metadata: EpiDataMetaSourceInfo[]): {
   map: Map<string, SensorSensor>;
   sources: SensorSource[];
 } {
-  const sources = metadata.map((sm): SensorSource => {
-    const credits = generateCredits(sm.license);
-    const sensors: SensorSensor[] = sm.signals.map((m) => {
-      const parsed = parse(m);
-      const s: SensorSensor = {
-        key: toKey(m.source, m.signal),
-        active: m.active,
-        id: m.source,
-        signal: m.signal,
-        name: m.name,
-        description: m.description ? parseMarkDown(m.description.trim()) : '',
-        signalTooltip: m.short_description ? parseInline(m.short_description) : '',
-        valueScaleFactor: m.format === 'fraction' ? 100 : 1,
-        format: m.format ?? 'raw',
-        highValuesAre: m.high_values_are ?? 'neutral',
-        hasStdErr: m.has_stderr,
-        is7DayAverage: m.is_smoothed,
-        dataSourceName: sm.name,
-        levels: Object.keys(m.geo_types) as RegionLevel[],
-        type: m.category ?? 'other',
-        xAxis: m.time_label,
-        yAxis: m.value_label || yAxis[m.format] || yAxis.raw,
-        unit: units[m.format] || units.raw,
-        colorScale: colorScales[m.high_values_are],
-        vegaColorScale: vegaColorScales[m.high_values_are],
-        links: sm.link.map((d) => `<a href="${d.href}">${d.alt}</a>`),
-        credits: credits,
-        formatValue: formatter[m.format] || formatter.raw,
-        formatSpecifier: formatSpecifiers[m.format] || formatSpecifiers.raw,
-        isWeeklySignal: parsed.time_type === 'week',
-        meta: parsed,
+  const sources = metadata
+    .map((sm): SensorSource => {
+      const credits = generateCredits(sm.license);
+      const sensors: SensorSensor[] = sm.signals.map((m) => {
+        const parsed = parse(m);
+        const s: SensorSensor = {
+          key: toKey(m.source, m.signal),
+          active: m.active,
+          id: m.source,
+          signal: m.signal,
+          name: m.name,
+          description: m.description ? parseMarkDown(m.description.trim()) : '',
+          signalTooltip: m.short_description ? parseInline(m.short_description) : '',
+          valueScaleFactor: m.format === 'fraction' ? 100 : 1,
+          format: m.format ?? 'raw',
+          highValuesAre: m.high_values_are ?? 'neutral',
+          hasStdErr: m.has_stderr,
+          is7DayAverage: m.is_smoothed,
+          dataSourceName: sm.name,
+          levels: Object.keys(m.geo_types) as RegionLevel[],
+          type: m.category ?? 'other',
+          xAxis: m.time_label,
+          yAxis: m.value_label || yAxis[m.format] || yAxis.raw,
+          unit: units[m.format] || units.raw,
+          colorScale: colorScales[m.high_values_are],
+          vegaColorScale: vegaColorScales[m.high_values_are],
+          links: sm.link.map((d) => `<a href="${d.href}">${d.alt}</a>`),
+          credits: credits,
+          formatValue: formatter[m.format] || formatter.raw,
+          formatSpecifier: formatSpecifiers[m.format] || formatSpecifiers.raw,
+          isWeeklySignal: parsed.time_type === 'week',
+          meta: parsed,
+        };
+        return s;
+      });
+
+      // inject raw versions and cumulative versions
+      for (const s of sensors) {
+        const raw = findRawSignal(s, sensors);
+        if (raw) {
+          Object.assign(s, {
+            rawSensor: raw,
+            rawSignal: raw.signal,
+          });
+        }
+        const cum = findRawCumulativeSignal(s, sensors);
+        if (cum) {
+          Object.assign(s, {
+            rawCumulativeSensor: cum,
+            rawCumulativeSignal: cum.signal,
+          });
+        }
+      }
+
+      return {
+        ...sm,
+        link: addDUALink(sm.link, sm.dua),
+        sensors,
+        description: sm.description ? parseMarkDown(sm.description.trim()) : '',
+        credits,
+        referenceSensor: sm.reference_signal ? sensors.find((d) => d.signal === sm.reference_signal) : undefined,
       };
-      return s;
-    });
-
-    // inject raw versions and cumulative versions
-    for (const s of sensors) {
-      const raw = findRawSignal(s, sensors);
-      if (raw) {
-        Object.assign(s, {
-          rawSensor: raw,
-          rawSignal: raw.signal,
-        });
-      }
-      const cum = findRawCumulativeSignal(s, sensors);
-      if (cum) {
-        Object.assign(s, {
-          rawCumulativeSensor: cum,
-          rawCumulativeSignal: cum.signal,
-        });
-      }
-    }
-
-    return {
-      ...sm,
-      link: addDUALink(sm.link, sm.dua),
-      sensors,
-      description: sm.description ? parseMarkDown(sm.description.trim()) : '',
-      credits,
-      referenceSensor: sm.reference_signal ? sensors.find((d) => d.signal === sm.reference_signal) : undefined,
-    };
-  });
+    })
+    .sort((a, b) => a.source.localeCompare(b.source));
 
   const sensors = sources.map((d) => d.sensors).flat();
   sensors.sort((a, b) => a.key.localeCompare(b.key));
