a4a26da83e350781a5474f72d5e21e9b22acea51
Kathryn Mazaitis <krivard@cs.cmu.edu>
2021-08-20 13:00:33 -0400

    More prettier

--- src/modes/indicator-status/IndicatorStatusTable.svelte
+++ src/modes/indicator-status/IndicatorStatusTable.svelte
@@ -74,9 +74,7 @@
       <th class="mobile-th uk-text-right" title="Date the most recent update was published">Latest Issue</th>
       <th class="mobile-th uk-text-right" title="Most recent date for which data is available">Latest Data</th>
       <th class="mobile-th uk-text-right">Lag to Today</th>
-      <th
-        class="mobile-th uk-text-right"
-        title="Percent of US counties included in latest day of data"
+      <th class="mobile-th uk-text-right" title="Percent of US counties included in latest day of data"
         >Latest County Coverage</th
       >
       <th class="mobile-th uk-text-right">
