4248fee0871dc2675c401e29414fd88165771303
Samuel Gratzl <sam@sgratzl.com>
2022-01-04 22:45:23 -0500

    feat: remove include zero toggle
    closes #1112

--- src/blocks/HistoryLineChart.svelte
+++ src/blocks/HistoryLineChart.svelte
@@ -95,14 +95,14 @@
    * @param {import('..stores/params').RegionParam} region
    * @param {import('../stores/params').DateParam} date
    * @param {import('../stores/params').TimeFrame} timeFrame
-   * @param {{height: number, zero: boolean, raw: boolean, isMobile: boolean, singleRegionOnly: boolean, stderr: boolean}} options
+   * @param {{height: number, raw: boolean, isMobile: boolean, singleRegionOnly: boolean, stderr: boolean}} options
    */
   function genSpec(
     sensor,
     region,
     date,
     timeFrame,
-    { height, zero, raw, isMobile, singleRegionOnly, domain, showNeighbors, stderr },
+    { height, raw, isMobile, singleRegionOnly, domain, showNeighbors, stderr },
   ) {
     const isWeekly = sensor.value.isWeeklySignal;
     const options = {
@@ -110,7 +110,7 @@
       height,
       color,
       domain: domain || timeFrame.domain,
-      zero,
+      zero: false,
       xTitle: sensor.xAxis,
       title: joinTitle([sensor.name, `in ${region.displayName}`], isMobile),
       subTitle: sensor.unit,
@@ -258,7 +258,6 @@
     return spec;
   }
 
-  let zero = false;
   export let stderr = false;
   let singleRaw = false;
 
@@ -268,7 +267,6 @@
   $: spec = injectRanges(
     genSpec(sensor, region, date, timeFrame, {
       height,
-      zero,
       raw,
       isMobile: $isMobileDevice,
       singleRegionOnly,
@@ -323,7 +321,6 @@
 />
 
 <div class="buttons">
-  <Toggle bind:checked={zero}>Include 0 in Y Axis</Toggle>
   {#if sensor.rawValue != null && !($isMobileDevice && showAllDates)}
     <Toggle bind:checked={singleRaw}>Raw Data</Toggle>
   {/if}
