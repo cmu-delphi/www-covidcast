4036d419b9eefb0c8048d98947320a463f6c10b9
george haff <george.haff@gmail.com>
2023-11-03 15:00:38 -0400

    temp disable of Overview widget

--- src/modes/summary/Summary.svelte
+++ src/modes/summary/Summary.svelte
@@ -70,7 +70,9 @@
   <div class="uk-container content-grid">
     <div class="grid-3-11">
       <FancyHeader invert>{region.displayName}</FancyHeader>
+      <!-- TEMPORARY DISABLING OF THIS OVERVIEW WIDGET UNTIL SIGNALS ARE FIXED
       <Overview {date} {region} {fetcher} />
+      -->
       <hr />
       <FancyHeader invert sub="Map" anchor="map">{HOSPITAL_ADMISSION.name}</FancyHeader>
       <p>{@html HOSPITAL_ADMISSION.signalTooltip}</p>
