0b42ad184deb1f1a7330d485e0f1761287220959
Dmytro Trotsko <dmytrotsko@gmail.com>
2024-04-23 18:54:25 +0300

    OKRS-24-55 (#1243)
    * 'latest known data' link now preserves context
    
    * Set default date to the date we have data for all 3 sensors.
    
    * Made Hospital Admissions sensor work as Doctor Visits and Deaths. Removed annotation for Hospital Admissions sensor when we don't have recent data for chosen date

--- src/modes/summary/Overview.svelte
+++ src/modes/summary/Overview.svelte
@@ -1,14 +1,12 @@
 <script>
   import KPIValue from '../../components/KPIValue.svelte';
   import KPIChange from '../../components/KPIChange.svelte';
-  import { DateParam, SensorParam } from '../../stores/params';
-  import { formatDateDayOfWeek } from '../../formats';
+  import { SensorParam } from '../../stores/params';
   import SensorUnit from '../../components/SensorUnit.svelte';
-  import IndicatorAnnotations from '../../components/IndicatorAnnotations.svelte';
   import MaxDateHint from '../../blocks/MaxDateHint.svelte';
-  import IndicatorWarning from '../../blocks/IndicatorWarning.svelte';
+  import NoRecentDataWarning from '../../blocks/NoRecentDataWarning.svelte';
   import { defaultDeathSensor, defaultCasesSensor, defaultHospitalSensor, metaDataManager } from '../../stores';
-  import IndicatorFallbackWarning from '../../blocks/IndicatorFallbackWarning.svelte';
+  import { onMount, beforeUpdate } from 'svelte';
 
   /**
    * @type {import("../../stores/params").DateParam}
@@ -27,33 +25,56 @@
   $: DEATHS = new SensorParam($defaultDeathSensor, $metaDataManager);
   $: HOSPITAL_ADMISSION = new SensorParam($defaultHospitalSensor, $metaDataManager);
 
-  function fetchFallback(fetcher, sensor, region, trend) {
-    return trend.then((t) => {
-      if (t && t.value != null) {
-        return t;
-      }
-      return fetcher.fetch1Sensor1Region1DateTrend(sensor, region, DateParam.box(sensor.timeFrame.max));
-    });
-  }
-
   $: trends = fetcher.fetchNSensors1Region1DateTrend([CASES, HOSPITAL_ADMISSION, DEATHS], region, date);
   $: casesTrend = trends[0];
-  $: hospitalTrend = fetchFallback(fetcher, HOSPITAL_ADMISSION, region, trends[1]);
+  $: hospitalTrend = trends[1];
   $: deathTrend = trends[2];
+
+  let minMaxDate = new Date();
+
+  onMount(() => {
+    [CASES, HOSPITAL_ADMISSION].map((s) => {
+      if (s.timeFrame.max < minMaxDate) {
+        minMaxDate = s.timeFrame.max;
+      }
+    });
+    let urlSearchParams = new URLSearchParams(window.location.search);
+    if (!urlSearchParams.has('date')) {
+      // if no date is specified in the URL, default to the latest day before today with data from all 3 highlighted indicators
+      date.set(minMaxDate);
+    }
+  });
+
+  // warningType is indicator of which exact warning message should be shown.
+  // By default, when user opens page with no specified date, the date will be set to the latest date we have data for all 3 indicators.
+  // In this case, warningType should be set to 1.
+  // In case selected date is set to future date (date > minMaxDate, where we don't have recent data for all 3 indicators), the warningType will be set to 2
+  // which has different warning message.
+  // In case selected date is set to some date which is < minMaxDate, the warningType will be set to 0 which means that we will not show
+  // any warning message.
+
+  // warningType should be set in beforeUpdate() method, to guess correct warningType.
+
+  let warningType = 1;
+  beforeUpdate(() => {
+    if (date.value > minMaxDate) {
+      warningType = 2;
+    } else if (date.value < minMaxDate) {
+      warningType = 0;
+    } else {
+      warningType = 1;
+    }
+  });
 </script>
 
-<IndicatorWarning sensor={CASES} {date} {region} {fetcher} />
-<IndicatorAnnotations {date} {region} sensor={CASES} range="sparkLine" />
-
-<p>
-  On {formatDateDayOfWeek(date.value)}
-  <MaxDateHint sensor={CASES.value} suffix="," {fetcher} />
-  the {CASES.valueUnit}s were:
-</p>
+<NoRecentDataWarning {minMaxDate} {date} {warningType} />
 
 <div class="mobile-three-col">
   <div class="mobile-kpi">
-    <h3>Doctor Visits</h3>
+    <h3>
+      Doctor Visits
+      <MaxDateHint sensor={CASES.value} {fetcher} />
+    </h3>
     <div>
       {#await casesTrend}
         <KPIValue value={null} loading />
@@ -66,12 +87,15 @@
     </div>
   </div>
   <div class="mobile-kpi">
-    <h3>Hospital Admissions</h3>
+    <h3>
+      Hospital Admissions
+      <MaxDateHint sensor={HOSPITAL_ADMISSION.value} {fetcher} />
+    </h3>
     <div>
       {#await hospitalTrend}
         <KPIValue value={null} loading />
       {:then d}
-        <KPIValue value={d ? d.value : null} asterisk={d != null && (d.value == null || d.date < date.value)} />
+        <KPIValue value={d ? d.value : null} />
       {/await}
     </div>
     <div class="sub">
@@ -79,7 +103,10 @@
     </div>
   </div>
   <div class="mobile-kpi">
-    <h3>Deaths</h3>
+    <h3>
+      Deaths
+      <MaxDateHint sensor={DEATHS.value} {fetcher} />
+    </h3>
     <div>
       {#await deathTrend}
         <KPIValue value={null} loading />
@@ -111,10 +138,7 @@
       {#await hospitalTrend}
         <KPIChange value={null} loading />
       {:then d}
-        <KPIChange
-          value={d && d.value != null && !Number.isNaN(d.value) ? d.change : null}
-          asterisk={d != null && (d.value == null || d.date < date.value)}
-        />
+        <KPIChange value={d && d.value != null && !Number.isNaN(d.value) ? d.change : null} />
       {/await}
     </div>
     <div class="sub">Relative change to 7 days ago</div>
@@ -130,5 +154,3 @@
     <div class="sub">Relative change to 7 days ago</div>
   </div>
 </div>
-
-<IndicatorFallbackWarning trend={hospitalTrend} date={date.value} level={region.level} sensor={HOSPITAL_ADMISSION} />
